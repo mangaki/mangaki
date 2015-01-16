26243455bc4abe7a503854b34afa90a9a1574a77
Jill-Jênn Vie <vie@jill-jenn.net>
2015-01-16 16:30:31 +0100

    Put DEBUG in secret

--- mangaki/mangaki/settings.py
+++ mangaki/mangaki/settings.py
@@ -11,14 +11,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from django.conf import global_settings
-from secret import SECRET_KEY, DISCOURSE_SSO_SECRET
+from secret import SECRET_KEY, DISCOURSE_SSO_SECRET, DEBUG
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 TEMPLATE_DEBUG = True
 
