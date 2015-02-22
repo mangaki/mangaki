7f08f13328f2ba9aa85b87a115e7feadf07fe3bb
Jill-Jênn Vie <vie@jill-jenn.net>
2015-02-22 16:49:56 +0100

    Add last activity

---
+++ mangaki/mangaki/management/commands/lastactivity.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+import datetime
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Displays last activity'
+    def handle(self, *args, **options):
+        for user, date in User.objects.order_by('-last_login').values_list('username', 'last_login')[:5]:
+            print(user, datetime.datetime.strftime(date, '%d/%m/%Y %H:%M:%S'))
