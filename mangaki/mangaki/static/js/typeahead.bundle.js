5767d6fa451ad6fba25bf8919e2b2477a5748092
Jill-Jênn Vie <vie@jill-jenn.net>
2015-04-30 15:01:18 +0200

    Fix typeahead layout

--- mangaki/mangaki/static/js/typeahead.bundle.js
+++ mangaki/mangaki/static/js/typeahead.bundle.js
@@ -738,7 +738,7 @@
         var css = {
             wrapper: {
                 position: "relative",
-                display: "inline-block"
+                display: "block"
             },
             hint: {
                 position: "absolute",
