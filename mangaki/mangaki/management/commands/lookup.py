83833e518207e44f331784bfe85a7c70c8e7171c
Jill-Jênn Vie <vie@jill-jenn.net>
2015-06-12 15:44:26 +0200

    Order lookup by rating count

--- mangaki/mangaki/management/commands/lookup.py
+++ mangaki/mangaki/management/commands/lookup.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from mangaki.models import Work, Rating
 from django.db import connection
+from django.db.models import Count
 from collections import Counter
 import sys
 
@@ -8,7 +9,7 @@ class Command(BaseCommand):
     args = ''
     help = 'Lookup some work'
     def handle(self, *args, **options):
-        work = Work.objects.filter(title__icontains=args[0])[0]
+        work = Work.objects.filter(title__icontains=args[0]).annotate(Count('rating')).order_by('-rating__count')[0]
         print(work.title)
         nb = Counter()
         for rating in Rating.objects.filter(work=work):
