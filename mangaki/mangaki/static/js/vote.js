68e9cbd19bd4d45c448f9d25c36c1a20f64dedf5
Jill-Jênn Vie <vie@jill-jenn.net>
2015-07-05 10:16:55 +0200

    Fix all clicks using data class

--- mangaki/mangaki/static/js/vote.js
+++ mangaki/mangaki/static/js/vote.js
@@ -1,11 +1,13 @@
 function getSheet(elt) {
-    entity = $(elt).closest('.thumbnail'); // See work_poster
+    entity = $(elt).closest('.data'); // See work_poster
     if(entity.data('category') != 'dummy')
         location.href = '/' + entity.data('category') + '/' + entity.data('id');
 }
 
 function vote(elt) {
-    entity = $(elt).closest('.thumbnail');
+    entity = $(elt).closest('.data');
+    console.log(entity);
+    console.log($(elt));
     work_id = entity.data('id');
     choice = $(elt).data('choice');
     pos = entity.data('pos');
@@ -13,7 +15,7 @@ function vote(elt) {
         if(rating == '')
             window.location = '/user/signup';
         dejaVu = $('[data-id]').map(function() {return $(this).data('id');}).get();
-        if(sort_mode == 'mosaic' && rating)
+        if(typeof(sort_mode) != 'undefined' && sort_mode == 'mosaic' && rating)
             loadCard(pos, dejaVu);
         else {
             $(elt).siblings().filter('[data-choice!=' + rating + ']').addClass('not-chosen');
@@ -56,7 +58,8 @@ function displayWork(pos, work) {
         display_votes = false;
     }
     selector = ':nth-child(' + pos + ')';
-    work_div = $('.manga-sheet' + selector + ' .thumbnail');
+    console.log('.manga-sheet' + selector + ' .data');
+    work_div = $('.manga-sheet' + selector + ' .data');
     work_div.data('category', work['category']);
     work_div.data('id', work['id']);
     work_div.find('h4 a').text(work['title']);
