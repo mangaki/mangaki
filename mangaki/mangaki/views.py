3d2c6678d663d8798246a98bc15746c5a140f1ec
Dylan Tanguy <dylan.tanguy@gmail.com>
2015-07-28 19:16:15 +0900

    bouton pour la newsletter : je sais pas si ça marche mais je sais pas comment tester, quelqu'un a une idée ?

--- mangaki/mangaki/views.py
+++ mangaki/mangaki/views.py
@@ -611,6 +611,11 @@ def update_nsfw(request):
         Profile.objects.filter(user=request.user).update(nsfw_ok=request.POST['nsfw_ok'] == 'true')
     return HttpResponse()
 
+def update_newsletter(request):
+    if request.user.is_authenticated() and request.method == 'POST':
+        Profile.objects.filter(user=request.user).update(newsletter_ok=request.POST['newsletter_ok'] == 'true')
+    return HttpResponse()
+
 def update_reco_willsee(request):
     if request.user.is_authenticated() and request.method == 'POST':
         Profile.objects.filter(user=request.user).update(reco_willsee_ok=request.POST['reco_willsee_ok'] == 'true')
