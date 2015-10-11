5f4bcff3d077be947b8e901351bd1ed17a074492
Jill-Jênn Vie <vie@jill-jenn.net>
2015-10-11 04:01:55 +0200

    Make top faster

--- mangaki/mangaki/management/commands/top.py
+++ mangaki/mangaki/management/commands/top.py
@@ -11,10 +11,12 @@ class Command(BaseCommand):
         category = 'composer'
         c = Counter()
         values = {'favorite': 2, 'like': 1, 'neutral': 0.5, 'dislike': 0}
-        anime_ids = Anime.objects.values_list('id', flat=True)
-        for rating in Rating.objects.select_related('work__anime__' + category):
-            if rating.work_id in anime_ids:
+        anime_ids = Anime.objects.filter(anidb_aid__gt=0).values_list('id', flat=True)
+        for rating in Rating.objects.filter(work_id__in=anime_ids).select_related('work__anime__' + category):
+            try:
                 c[getattr(rating.work.anime, category)] += values.get(rating.choice, 0)
-        for i, k, v in enumerate(c.most_common(10)):
+            except:
+                print(rating)
+        for i, (k, v) in enumerate(c.most_common(10)):
             print('%d.' % (i + 1), k, v)
         print(len(connection.queries), 'queries')
