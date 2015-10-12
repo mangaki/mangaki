a3f6bcc7ed072d4a96c559bb6eabd7ac4847d744
Jill-Jênn Vie <vie@jill-jenn.net>
2015-10-12 18:23:25 +0200

    Top has now category

--- mangaki/mangaki/management/commands/top.py
+++ mangaki/mangaki/management/commands/top.py
@@ -4,15 +4,16 @@ from django.db import connection
 from mangaki.models import Rating, Anime
 from collections import Counter
 import json
+import sys
 
 class Command(BaseCommand):
     args = ''
     help = 'Builds top'
     def handle(self, *args, **options):
-        category = 'composer'
+        category = sys.argv[2]
         c = Counter()
         values = {'favorite': 10, 'like': 2, 'neutral': 0.5, 'dislike': -1}
-        anime_ids = Anime.objects.exclude(composer=1).values_list('id', flat=True)
+        anime_ids = Anime.objects.exclude(**{category: 1}).values_list('id', flat=True)
         nb_ratings = Counter()
         nb_stars = Counter()
         for rating in Rating.objects.filter(work_id__in=anime_ids).select_related('work__anime__' + category):
