ba2771f707800017777d4dfb3f8970d009c47bfd
Raito Bezarius <masterancpp@gmail.com>
2015-12-29 17:05:15 +0100

    Add a JWT Token route to get some juicy tokens

--- mangaki/mangaki/urls.py
+++ mangaki/mangaki/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from mangaki.router import router
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'api/', include('rest_framework.urls'))
+    url(r'api/', include('rest_framework.urls')),
+    url(r'auth/', obtain_jwt_token)
 ]
