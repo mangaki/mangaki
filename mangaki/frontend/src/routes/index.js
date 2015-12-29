9e57a1361ab3e9bcd9a842e922d889984849f5ac
Raito Bezarius <masterancpp@gmail.com>
2015-12-29 19:10:37 +0100

    Add the login view route

--- mangaki/frontend/src/routes/index.js
+++ mangaki/frontend/src/routes/index.js
@@ -9,10 +9,12 @@ import { Route, IndexRoute } from 'react-router'
 import CoreLayout from 'layouts/CoreLayout'
 import HomeView from 'views/HomeView'
 import AboutView from 'views/AboutView'
+import LoginView from 'views/LoginView'
 
 export default (
   <Route path='/' component={CoreLayout}>
     <IndexRoute component={HomeView} />
     <Route path='/about' component={AboutView} />
+    <Route path='/user/login' component={LoginView} />
   </Route>
 )
