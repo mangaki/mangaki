9ba1600fbe6ad78443b253e7e6e283811cb37d43
Raito Bezarius <masterancpp@gmail.com>
2015-12-29 17:04:55 +0100

    Add REST Framework configuration with JWT tokens

--- mangaki/mangaki/settings.py
+++ mangaki/mangaki/settings.py
@@ -61,6 +61,17 @@ TEMPLATES = [
     }
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+}
+
 ROOT_URLCONF = 'mangaki.urls'
 
 WSGI_APPLICATION = 'mangaki.wsgi.application'
