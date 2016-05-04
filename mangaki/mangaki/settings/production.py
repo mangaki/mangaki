68a3c00c6b151c5890e4f71282d10de2e6a4c6ee
Raito Bezarius <masterancpp@gmail.com>
2016-05-04 21:15:19 +0200

    mangaki(settings): remove magic and implement simpler strategy use DJANGO_SETTINGS_MODULE to target the settings module: DJANGO_SETTINGS_MODULE='mangaki.settings.production'
    import common by default from every modules, and import dev by default for the default settings.

---
+++ mangaki/mangaki/settings/production.py
@@ -0,0 +1,25 @@
+"""
+Production specific settings for Mangaki.
+
+Optimized for performance.
+"""
+
+from .common import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['mangaki.fr']
+
+INSTALLED_APPS += (
+    'allauth.socialaccount.providers.google', # Google Provider
+    'allauth.socialaccount.providers.twitter', # Twitter Provider
+    'allauth.socialaccount.providers.facebook', # Facebook Provider
+    'analytical', # Google Analytics helper (analytics service)
+)
+
+
+# Discourse settings
+DISCOURSE_BASE_URL = 'http://meta.mangaki.fr'
+
+# Google Analytics
+GOOGLE_ANALYTICS_PROPERTY_ID = 'UA-63869890-1'
