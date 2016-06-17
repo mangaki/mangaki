b64eee96e473f9b441f1b2aa3e13e0a216eeb64c
fravoi <voisinf@gmail.com>
2016-06-17 17:37:00 +0200

    notebook _notebook stage_ : meilleure implémentation de cosine, début de jaccard, notebook _essai_ : essais, début dimaètre d'ordre r

--- mangaki/mangaki/utils/dpp.py
+++ mangaki/mangaki/utils/dpp.py
@@ -1,8 +1,10 @@
 
 #similarity : matrice toute faite, à aprtir de cosine ou jaccard
-class MangakiDPP(self, similarity):
-	def constructeur :
+class MangakiDPP(object):
+	
+	def __init__(self, similarity) :
 		pass
 		#à compléter
 
-	def sample_k:
+	def sample_k(items, similiarity, k, max_nb_iterations=1000, rng=np.random):
+    #recopier
