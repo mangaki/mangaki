ffbdf97a128b1b46c6c4f92e6b3de24df918e1b3
Jill-Jênn Vie <vie@jill-jenn.net>
2016-06-30 22:38:15 +0200

    Add MEDIA_URL, MEDIA_ROOT to settings.py

--- mangaki/mangaki/settings.py
+++ mangaki/mangaki/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = config.get('deployment', 'STATIC_ROOT', fallback=os.path.join(BASE_DIR, 'static'))
+MEDIA_ROOT = config.get('deployment', 'MEDIA_ROOT', fallback=os.path.join(BASE_DIR, 'media'))
 
 STATIC_ROOT = config.get('deployment', 'STATIC_ROOT', fallback=os.path.join(BASE_DIR, 'static'))
 
