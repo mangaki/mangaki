e2c81d7944606a59ba8fd75a71f411c1ae0d71f5
Raito Bezarius <masterancpp@gmail.com>
2016-07-03 01:14:05 +0200

    factories: add a Work factory and use create rather than build

--- mangaki/mangaki/factories.py
+++ mangaki/mangaki/factories.py
@@ -1,7 +1,7 @@
 import factory
 from factory.django import DjangoModelFactory
 
-from .models import Profile
+from .models import Profile, Work, Category
 from django.contrib.auth.models import User
 
 class ProfileFactory(DjangoModelFactory):
@@ -23,8 +23,24 @@ class UserFactory(DjangoModelFactory):
     email = factory.LazyAttribute(lambda o: '{}@mangaki.fr'.format(o.username))
     profile = factory.RelatedFactory(ProfileFactory, 'user')
 
+class WorkFactory(DjangoModelFactory):
+    class Meta:
+        model = Work
+
+    category = factory.Iterator(Category.objects.all())
+
+    @factory.iterator
+    def title():
+        qs = Work.objects.values_list('title', flat=True).all()[:20]
+        for title in qs:
+            yield title
+
+    nsfw = factory.Faker('boolean')
+    synopsis = factory.Faker('text')
+
+
 def create_user(**kwargs):
-    return UserFactory.build(**kwargs)
+    return UserFactory.create(**kwargs)
 
 def create_user_with_profile(**kwargs):
     profile = kwargs.pop('profile')
