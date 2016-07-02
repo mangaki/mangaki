98bc1ffe6ac44765c7233f062d2b53e8ed39cffa
Raito Bezarius <masterancpp@gmail.com>
2016-07-03 01:20:09 +0200

    models(work): add a save handler for works without categories

--- mangaki/mangaki/models.py
+++ mangaki/mangaki/models.py
@@ -122,7 +122,6 @@ class Work(models.Model):
     def __str__(self):
         return self.title
 
-
 class Role(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
