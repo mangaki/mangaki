83078e1700aa86f928552aa3ffe5d676184a15a3
Raito Bezarius <masterancpp@gmail.com>
2016-07-02 18:10:19 +0200

    migrations(0036_trgm): add CreateExtension / UnaccentExtension operations to enable testing
    In order to make testing possible (which create test databases on the
    fly), we have to add these operations.
    
    Side-effect: the user have to be super-user in order to create those
    extensions.

--- mangaki/mangaki/migrations/0036_trgm.py
+++ mangaki/mangaki/migrations/0036_trgm.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
+from django.contrib.postgres.operations import CreateExtension, UnaccentExtension
 
 
 class Migration(migrations.Migration):
@@ -12,16 +13,18 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL("""
-        CREATE OR REPLACE FUNCTION F_UNACCENT(text) RETURNS text AS
-        $func$
-        SELECT public.unaccent('public.unaccent', $1)
-        $func$ LANGUAGE sql IMMUTABLE;
-        CREATE INDEX mangaki_search_work_upper ON mangaki_work USING gist(UPPER(F_UNACCENT(title)) gist_trgm_ops);
-        """,
-        """
-        DROP INDEX mangaki_search_work_upper;
-        DROP FUNCTION F_UNACCENT(text);
-        """
-        ),
+            UnaccentExtension(),
+            CreateExtension('pg_trgm'),
+            migrations.RunSQL("""
+            CREATE OR REPLACE FUNCTION F_UNACCENT(text) RETURNS text AS
+            $func$
+            SELECT public.unaccent('public.unaccent', $1)
+            $func$ LANGUAGE sql IMMUTABLE;
+            CREATE INDEX mangaki_search_work_upper ON mangaki_work USING gist(UPPER(F_UNACCENT(title)) gist_trgm_ops);
+            """,
+            """
+            DROP INDEX mangaki_search_work_upper;
+            DROP FUNCTION F_UNACCENT(text);
+            """
+            ),
     ]
