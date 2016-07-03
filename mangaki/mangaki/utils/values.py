07014a3d508f17c8a2f20438de3c5c7868fa68eb
Jill-Jênn Vie <vie@jill-jenn.net>
2016-07-03 11:22:14 +0200

    Do not use binary rating values in production!

--- mangaki/mangaki/utils/values.py
+++ mangaki/mangaki/utils/values.py
@@ -1,2 +1,2 @@
-# rating_values = {'favorite': 4, 'like': 2, 'dislike': -2, 'neutral': 0.1, 'willsee': 0.5, 'wontsee': -0.5}
-rating_values = {'favorite': 1, 'like': 1, 'neutral': 0, 'dislike': 0, 'willsee': 0, 'wontsee': 0}  # For NMF
+rating_values = {'favorite': 4, 'like': 2, 'dislike': -2, 'neutral': 0.1, 'willsee': 0.5, 'wontsee': -0.5}
+# rating_values = {'favorite': 1, 'like': 1, 'neutral': 0, 'dislike': 0, 'willsee': 0, 'wontsee': 0}  # For NMF, do not use in production!
