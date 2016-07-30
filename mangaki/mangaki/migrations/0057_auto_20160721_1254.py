080591581c55c782ef168305edf798f52891c043
fravoi <voisinf@gmail.com>
2016-07-30 11:04:59 +0200

    Changement des dépendances des migrations pour que ça marche (management command marche et la loaddata de la seed aussi (mais pas la big seed :/))

--- mangaki/mangaki/migrations/0057_auto_20160721_1254.py
+++ mangaki/mangaki/migrations/0057_auto_20160721_1254.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('mangaki', '0056_merge'),
+        ('mangaki', '0057_auto_20160716_1417'),
     ]
 
     operations = [
