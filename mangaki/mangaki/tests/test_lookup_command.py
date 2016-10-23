09e660d05bd542f1517ce8e22b3c29af08f7fd4e
Basile Clement <basile@clement.pm>
2016-10-23 20:53:43 +0200

    Make tests pass, and actually run them on CircleCI.

--- mangaki/mangaki/tests/test_lookup_command.py
+++ mangaki/mangaki/tests/test_lookup_command.py
@@ -2,10 +2,15 @@ from django.core.management import call_command
 from django.test import TestCase
 from django.utils.six import StringIO
 
-from mangaki.models import Work, Category
+from mangaki.models import Work, Category, Editor, Studio
 
 class LookupTest(TestCase):
     def setUp(self):
+        # FIXME: The defaults for editor and studio in Work requires those to
+        # exist, or else foreign key constraints fail.
+        Editor.objects.create(pk=1)
+        Studio.objects.create(pk=1)
+
         anime = Category.objects.get(slug='anime')
         Work.objects.create(
             title='Steins;Gate',
