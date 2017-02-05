b5e42ab561a5441f63f258413d563a3675b7c826
Ryan Lahfa <masterancpp@gmail.com>
2017-02-06 00:49:42 +0100

    Reference local variable `now` properly before the loop
    Signed-off-by: Raito Bezarius <masterancpp@gmail.com>

--- mangaki/mangaki/management/commands/retrieveposters.py
+++ mangaki/mangaki/management/commands/retrieveposters.py
@@ -29,6 +29,7 @@ class Command(BaseCommand):
                     if work.int_poster.storage.exists(work.int_poster.name):
                         continue
 
+                now = time.time()
                 while len(recent) >= options['ratelimit']:
                     now = time.time()
                     recent = [t for t in recent if now - t < 1.]
