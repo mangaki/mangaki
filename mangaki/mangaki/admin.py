67516dde4dc200f7923e51bcdfc65bf985c615f9
Ryan Lahfa <masterancpp@gmail.com>
2017-04-11 08:27:14 +0200

    Reformat and remove useless classes in admin (#294)
    * admin: reformat code and remove useless classes
    
    * admin: improve code style

--- mangaki/mangaki/admin.py
+++ mangaki/mangaki/admin.py
@@ -3,26 +3,14 @@ from django.contrib.admin import helpers
 from django.template.response import TemplateResponse
 from django.db.models import Count
 
-from mangaki.models import Work, TaggedWork, WorkTitle, Genre, Track, Tag, Artist, Studio, Editor, Rating, Page, Suggestion, SearchIssue, Announcement, Recommendation, Pairing, Reference, Top, Ranking, Role, Staff, FAQTheme, FAQEntry
+from mangaki.models import (
+    Work, TaggedWork, WorkTitle, Genre, Track, Tag, Artist, Studio, Editor, Rating, Page,
+    Suggestion, SearchIssue, Announcement, Recommendation, Pairing, Reference, Top, Ranking,
+    Role, Staff, FAQTheme,
+    FAQEntry
+)
 from mangaki.utils.anidb import AniDB
 from mangaki.utils.db import get_potential_posters
-
-
-class TagAdmin(admin.ModelAdmin):
-    list_display = ("title", )
-    readonly_fields = ("nb_works_linked",)
-
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        return qs.annotate(works_linked=Count('work'))
-
-    def nb_works_linked(self, obj):
-        return obj.works_linked
-    nb_works_linked.short_description = 'Nombre d\'oeuvres liées au tag'
-
-
-class TaggedWorkAdmin(admin.ModelAdmin):
-    search_fields = ('work', 'tag')
 
 
 class TaggedWorkInline(admin.TabularInline):
@@ -41,11 +29,11 @@ class WorkTitleInline(admin.TabularInline):
 
 
 class AniDBaidListFilter(admin.SimpleListFilter):
-    title = ('AniDB aid')
+    title = 'AniDB aid'
     parameter_name = 'AniDB aid'
 
     def lookups(self, request, model_admin):
-        return (('Vrai', ('Oui')), ('Faux', ('Non')))
+        return ('Vrai', 'Oui'), ('Faux', 'Non')
 
     def queryset(self, request, queryset):
         if self.value() == 'Faux':
@@ -56,16 +44,18 @@ class AniDBaidListFilter(admin.SimpleListFilter):
             return queryset
 
 
+@admin.register(FAQTheme)
 class FAQAdmin(admin.ModelAdmin):
-    ordering = ('order', )
-    search_fields = ('theme', )
+    ordering = ('order',)
+    search_fields = ('theme',)
     list_display = ('theme', 'order')
 
 
+@admin.register(Work)
 class WorkAdmin(admin.ModelAdmin):
     search_fields = ('id', 'title')
     list_display = ('id', 'title', 'nsfw')
-    list_filter = ('category', 'nsfw', AniDBaidListFilter, )
+    list_filter = ('category', 'nsfw', AniDBaidListFilter,)
     actions = ['make_nsfw', 'make_sfw', 'merge', 'refresh_work', 'update_tags_via_anidb']
     inlines = [StaffInline, WorkTitleInline, TaggedWorkInline]
     readonly_fields = (
@@ -83,6 +73,7 @@ class WorkAdmin(admin.ModelAdmin):
         else:
             message_bit = "%s œuvres sont" % rows_updated
         self.message_user(request, "%s désormais NSFW." % message_bit)
+
     make_nsfw.short_description = "Rendre NSFW les œuvres sélectionnées"
 
     # FIXME : https://github.com/mangaki/mangaki/issues/205
@@ -104,12 +95,18 @@ class WorkAdmin(admin.ModelAdmin):
 
         for anime in queryset.select_related("category"):
             if anime.category.slug != 'anime':
-                self.message_user(request, "%s n'est pas un anime. La recherche des tags via AniDB n'est possible que pour les animes " % anime.title)
+                self.message_user(request,
+                                  "%s n'est pas un anime. La recherche des tags via AniDB n'est possible que pour les "
+                                  "animes " % anime.title)
                 self.message_user(request, "Vous avez un filtre à votre droite pour avoir les animes avec un anidb_aid")
                 return None
             elif not anime.anidb_aid:
-                self.message_user(request, "%s n'a pas de lien actuel avec la base d'aniDB (pas d'anidb_aid)" % anime.title)
-                self.message_user(request, "Vous avez un filtre à votre droite pour avoir les animes avec un anidb_aid")
+                self.message_user(
+                    request,
+                    "%s n'a pas de lien actuel avec la base d'aniDB (pas d'anidb_aid)" % anime.title)
+                self.message_user(
+                    request,
+                    "Vous avez un filtre à votre droite pour avoir les animes avec un anidb_aid")
                 return None
 
         all_information = {}
@@ -120,13 +117,16 @@ class WorkAdmin(admin.ModelAdmin):
             added_tags = retrieve_tags["added_tags"]
             updated_tags = retrieve_tags["updated_tags"]
             kept_tags = retrieve_tags["kept_tags"]
-            all_information[anime.id] = {'title': anime.title, 'deleted_tags': deleted_tags.items(), 'added_tags': added_tags.items(), 'updated_tags': updated_tags.items(), "kept_tags": kept_tags.items()}
+            all_information[anime.id] = {'title': anime.title, 'deleted_tags': deleted_tags.items(),
+                                         'added_tags': added_tags.items(), 'updated_tags': updated_tags.items(),
+                                         "kept_tags": kept_tags.items()}
 
         context = {
-                   'all_information': all_information.items(),
-                   'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
-                  }
+            'all_information': all_information.items(),
+            'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
+        }
         return TemplateResponse(request, "admin/update_tags_via_anidb.html", context)
+
     update_tags_via_anidb.short_description = "Mise à jour des tags des oeuvres sélectionnées"
 
     def make_sfw(self, request, queryset):
@@ -136,6 +136,7 @@ class WorkAdmin(admin.ModelAdmin):
         else:
             message_bit = "%s œuvres ne sont" % rows_updated
         self.message_user(request, "%s désormais plus NSFW." % message_bit)
+
     make_sfw.short_description = "Rendre SFW les œuvres sélectionnées"
 
     def merge(self, request, queryset):
@@ -164,6 +165,7 @@ class WorkAdmin(admin.ModelAdmin):
             'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME
         }
         return TemplateResponse(request, 'admin/merge_selected_confirmation.html', context)
+
     merge.short_description = "Fusionner les œuvres sélectionnées"
 
     def refresh_work(self, request, queryset):
@@ -176,7 +178,9 @@ class WorkAdmin(admin.ModelAdmin):
                 if obj.retrieve_poster(chosen_poster):
                     downloaded_titles.append(obj.title)
             if downloaded_titles:
-                self.message_user(request, "Des posters ont été trouvés pour les anime suivants : %s." % ', '.join(downloaded_titles))
+                self.message_user(
+                    request,
+                    "Des posters ont été trouvés pour les anime suivants : %s." % ', '.join(downloaded_titles))
             else:
                 self.message_user(request, "Aucun poster n'a été trouvé, essayez de changer le titre.")
             return None
@@ -190,37 +194,36 @@ class WorkAdmin(admin.ModelAdmin):
             'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME
         }
         return TemplateResponse(request, 'admin/refresh_poster_confirmation.html', context)
+
     refresh_work.short_description = "Mettre à jour la fiche de l'anime (poster)"
 
 
-class GenreAdmin(admin.ModelAdmin):
-    pass
-
-
-class TrackAdmin(admin.ModelAdmin):
-    pass
-
-
+@admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     search_fields = ('id', 'name')
 
 
-class StudioAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    readonly_fields = ("nb_works_linked",)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(works_linked=Count('work'))
+
+    def nb_works_linked(self, obj):
+        return obj.works_linked
+
+    nb_works_linked.short_description = 'Nombre d\'oeuvres liées au tag'
 
 
-class EditorAdmin(admin.ModelAdmin):
-    pass
+@admin.register(TaggedWork)
+class TaggedWorkAdmin(admin.ModelAdmin):
+    search_fields = ('work', 'tag')
 
 
-class RatingAdmin(admin.ModelAdmin):
-    pass
-
-
-class PageAdmin(admin.ModelAdmin):
-    pass
-
-
+@admin.register(Suggestion)
 class SuggestionAdmin(admin.ModelAdmin):
     list_display = ('work', 'problem', 'date', 'user', 'is_checked')
     list_filter = ('problem',)
@@ -241,6 +244,7 @@ class SuggestionAdmin(admin.ModelAdmin):
         else:
             message_bit = "%s suggestions" % rows_updated
         self.message_user(request, "La validation de %s a réussi." % message_bit)
+
     check_suggestions.short_description = "Valider les suggestions sélectionnées"
 
     def uncheck_suggestions(self, request, queryset):
@@ -250,21 +254,21 @@ class SuggestionAdmin(admin.ModelAdmin):
         else:
             message_bit = "%s suggestions" % rows_updated
         self.message_user(request, "L'invalidation de %s a réussi." % message_bit)
+
     uncheck_suggestions.short_description = "Invalider les suggestions sélectionnées"
 
 
+@admin.register(SearchIssue)
 class SearchIssueAdmin(admin.ModelAdmin):
     list_display = ('title', 'date', 'user')
 
 
+@admin.register(Announcement)
 class AnnouncementAdmin(admin.ModelAdmin):
     exclude = ('title',)
 
 
-class RecommendationAdmin(admin.ModelAdmin):
-    pass
-
-
+@admin.register(Pairing)
 class PairingAdmin(admin.ModelAdmin):
     list_display = ('artist', 'work', 'date', 'user', 'is_checked')
     actions = ['make_director', 'make_composer', 'make_author']
@@ -273,7 +277,8 @@ class PairingAdmin(admin.ModelAdmin):
         rows_updated = 0
         director = Role.objects.get(slug='director')
         for pairing in queryset:
-            _, created = Staff.objects.get_or_create(work_id=pairing.work_id, artist_id=pairing.artist_id, role=director)
+            _, created = Staff.objects.get_or_create(work_id=pairing.work_id, artist_id=pairing.artist_id,
+                                                     role=director)
             if created:
                 pairing.is_checked = True
                 pairing.save()
@@ -283,13 +288,15 @@ class PairingAdmin(admin.ModelAdmin):
         else:
             message_bit = "%s réalisateurs ont" % rows_updated
         self.message_user(request, "%s été mis à jour." % message_bit)
+
     make_director.short_description = "Valider les appariements sélectionnés pour réalisation"
 
     def make_composer(self, request, queryset):
         rows_updated = 0
         composer = Role.objects.get(slug='composer')
         for pairing in queryset:
-            _, created = Staff.objects.get_or_create(work_id=pairing.work_id, artist_id=pairing.artist_id, role=composer)
+            _, created = Staff.objects.get_or_create(work_id=pairing.work_id, artist_id=pairing.artist_id,
+                                                     role=composer)
             if created:
                 pairing.is_checked = True
                 pairing.save()
@@ -299,6 +306,7 @@ class PairingAdmin(admin.ModelAdmin):
         else:
             message_bit = "%s compositeurs ont" % rows_updated
         self.message_user(request, "%s été mis à jour." % message_bit)
+
     make_composer.short_description = "Valider les appariements sélectionnés pour composition"
 
     def make_author(self, request, queryset):
@@ -315,9 +323,11 @@ class PairingAdmin(admin.ModelAdmin):
         else:
             message_bit = "%s auteurs ont" % rows_updated
         self.message_user(request, "%s été mis à jour." % message_bit)
+
     make_author.short_description = "Valider les appariements sélectionnés pour écriture"
 
 
+@admin.register(Reference)
 class ReferenceAdmin(admin.ModelAdmin):
     list_display = ['work', 'url']
 
@@ -331,6 +341,7 @@ class RankingInline(admin.TabularInline):
         return str(instance.content_object)
 
 
+@admin.register(Top)
 class TopAdmin(admin.ModelAdmin):
     inlines = [
         RankingInline,
@@ -341,27 +352,17 @@ class TopAdmin(admin.ModelAdmin):
         return False
 
 
+@admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
     model = Role
     prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Work, WorkAdmin)
-admin.site.register(Genre, GenreAdmin)
-admin.site.register(Track, TrackAdmin)
-admin.site.register(Artist, ArtistAdmin)
-admin.site.register(Studio, StudioAdmin)
-admin.site.register(Editor, EditorAdmin)
-admin.site.register(Rating, RatingAdmin)
-admin.site.register(Page, PageAdmin)
-admin.site.register(Suggestion, SuggestionAdmin)
-admin.site.register(SearchIssue, SearchIssueAdmin)
-admin.site.register(Announcement, AnnouncementAdmin)
-admin.site.register(Recommendation, RecommendationAdmin)
-admin.site.register(Pairing, PairingAdmin)
-admin.site.register(Reference, ReferenceAdmin)
-admin.site.register(Top, TopAdmin)
-admin.site.register(Role, RoleAdmin)
-admin.site.register(Tag, TagAdmin)
-admin.site.register(TaggedWork, TaggedWorkAdmin)
-admin.site.register(FAQTheme, FAQAdmin)
+
+admin.site.register(Genre)
+admin.site.register(Track)
+admin.site.register(Studio)
+admin.site.register(Editor)
+admin.site.register(Rating)
+admin.site.register(Page)
 admin.site.register(FAQEntry)
+admin.site.register(Recommendation)
