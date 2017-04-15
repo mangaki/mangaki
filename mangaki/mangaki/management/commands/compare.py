4a43a23d602441d553424d905e64f246a8937ad8
Jill-Jênn Vie <vie@jill-jenn.net>
2017-04-15 19:49:36 +0900

    Finally manage those damn logs

--- mangaki/mangaki/management/commands/compare.py
+++ mangaki/mangaki/management/commands/compare.py
@@ -15,52 +15,61 @@ from mangaki.utils.svd import MangakiSVD
 from mangaki.utils.pca import MangakiPCA
 from mangaki.utils.zero import MangakiZero
 from mangaki.utils.values import rating_values
+import logging
 
 
 NB_SPLIT = 5  # Divide the dataset into 5 buckets
+FILENAMES = {
+    'movies': 'ratings-ml.csv',
+    'mangas': 'ratings.csv'
+}
+CONVERT_FUNCTIONS = {
+    'movies': float,
+    'mangas': lambda choice: rating_values[choice]
+}
 
 
 class Experiment(object):
     def __init__(self, dataset_name):
         self.algos = [
-            lambda: MangakiALS(20),
+            # lambda: MangakiALS(20),
             # lambda: MangakiWALS(20),
-            lambda: MangakiSVD(20),
+            # lambda: MangakiSVD(20),
             # lambda: MangakiPCA(20),
             # lambda: MangakiKNN(20),
-            # lambda: MangakiZero()
+            lambda: MangakiZero()
         ]
         self.anonymized = None
         self.load_dataset(dataset_name)
-        self.compare_models()
+        self.logger = logging.getLogger(__name__)
 
     def load_dataset(self, dataset_name):
         dataset = Dataset()
-        if dataset_name == 'movies':
-            dataset.load_csv('ratings-ml.csv')
-        else:
-            dataset.load_csv('ratings.csv', convert=lambda choice: rating_values[choice])
+        dataset.load_csv(
+            FILENAMES.get(dataset_name, FILENAMES['mangas']),
+            CONVERT_FUNCTIONS.get(dataset_name, CONVERT_FUNCTIONS['mangas'])
+        )
         self.anonymized = dataset.anonymized
 
     def compare_models(self):
         k_fold = ShuffleSplit(n_splits=NB_SPLIT)
-        rmse_values = defaultdict(lambda: [])
+        rmse_values = defaultdict(list)
         for i_train, i_test in k_fold.split(self.anonymized.X):
             for algo in self.algos:
                 model = algo()
-                print(model.get_shortname())
+                self.logger.info(model.get_shortname())
                 model.set_parameters(self.anonymized.nb_users, self.anonymized.nb_works)
                 model.fit(self.anonymized.X[i_train], self.anonymized.y[i_train])
                 y_pred = model.predict(self.anonymized.X[i_test])
                 rmse = model.compute_rmse(y_pred, self.anonymized.y[i_test])
                 if model.verbose:
-                    print('Predicted:', y_pred[:5])
-                    print('Was:', self.anonymized.y[i_test][:5])
-                print('RMSE', rmse)
+                    self.logger.debug('Predicted:', y_pred[:5])
+                    self.logger.debug('Was: %s' % self.anonymized.y[i_test][:5])
+                self.logger.debug('RMSE %f' % rmse)
                 rmse_values[model.get_shortname()].append(rmse)
-        print('Final results')
+        self.logger.info('Final results')
         for algo_name in rmse_values:
-            print('%s: RMSE = %f' % (algo_name, np.mean(rmse_values[algo_name])))
+            self.logger.info('%s: RMSE = %f' % (algo_name, np.mean(rmse_values[algo_name])))
 
 
 class Command(BaseCommand):
@@ -73,3 +82,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         dataset_name = options.get('dataset_name')
         experiment = Experiment(dataset_name)
+        experiment.compare_models()
--- mangaki/mangaki/settings.py
+++ mangaki/mangaki/settings.py
@@ -119,6 +119,28 @@ TEMPLATES = [
     }
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log'
+        }
+    },
+    'loggers': {
+        'mangaki': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG'
+        }
+    },
+}
+
 ROOT_URLCONF = 'mangaki.urls'
 WSGI_APPLICATION = 'mangaki.wsgi.application'
 
