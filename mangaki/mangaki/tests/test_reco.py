67fc9402328e7b9524da321e84ceaaed51e30d5b
Jill-Jênn Vie <vie@jill-jenn.net>
2017-04-16 20:08:19 +0900

    Oops, forgot one file

--- mangaki/mangaki/tests/test_reco.py
+++ mangaki/mangaki/tests/test_reco.py
@@ -1,20 +1,12 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-<<<<<<< HEAD
 from django.contrib.auth import get_user_model
-=======
-from django.contrib.auth.models import User
->>>>>>> master
 
 
 class RecoTest(TestCase):
 
     def setUp(self):
-<<<<<<< HEAD
         get_user_model().objects.create_user(username='test', password='test')
-=======
-        User.objects.create_user(username='test', password='test')
->>>>>>> master
 
     def test_reco(self, **kwargs):
         self.client.login(username='test', password='test')
