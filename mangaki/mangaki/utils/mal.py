56284fab231692092d3402ff6824f6f933e8e7bb
Raito Bezarius <masterancpp@gmail.com>
2017-04-22 16:49:40 +0200

    mal-import: refactor the whole code into something more maintainable
    - Fix doctor's fields and use MALEntry.
    - MAL:
            - Refactor the whole module using a requests.Session and
              instantiating a MALClient which handles the API operations.
            - Rewrite the DB interaction to be more efficient and fix it in
              some edge cases.

--- mangaki/mangaki/utils/mal.py
+++ mangaki/mangaki/utils/mal.py
@@ -4,6 +4,9 @@ looking for anime and finding URL of posters.
 """
 
 import xml.etree.ElementTree as ET
+from enum import Enum
+from typing import Optional, List
+
 import requests
 import html
 import re
@@ -13,7 +16,13 @@ from random import randint
 from django.conf import settings
 
 from django.contrib.auth.models import User
-from mangaki.models import Work, Rating, SearchIssue, Artist, Category
+from django.db.models import QuerySet, Q
+
+from mangaki.models import Work, Rating, SearchIssue, Category
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def _encoding_translation(text):
@@ -31,149 +40,252 @@ def random_ip():
     return '.'.join(map(str, [randint(100, 255) for _ in range(4)]))
 
 
-def retrieve_anime(entries):
-    unknown = Artist.objects.get(id=1)
-    anime_cat = Category.objects.get(slug='anime')
-    for entry in entries:
-        if Work.objects.filter(category=anime_cat, ext_poster=entry['image']).count() == 0:  # SCANDALE
-            title = entry['english'] if entry['english'] else entry['title']
-            if '0000' in entry['start_date']:
-                anime_date = None
-            elif '-00-00' in entry['start_date']:
-                anime_date = entry['start_date'].replace('-00-00', '-01-01')
-            elif '-00' in entry['start_date']:
-                anime_date = entry['start_date'].replace('-00', '-01')
-            else:
-                anime_date = entry['start_date']
-            Work.objects.create(category=anime_cat, title=title, source='http://myanimelist.net/anime/' + entry['id'], ext_poster=entry['image'], date=anime_date)
+class MALWorks(Enum):
+    animes = 'anime'
+    mangas = 'manga'
 
 
-def lookup_mal_api(query):
-    """
-    Run a query on MyAnimeList using its XML search API.
-    """
+class MALEntry:
+    def __init__(self, xml_entry, work_type: MALWorks):
+        self.xml = xml_entry
+        self.raw_properties = {child.tag: child.text for child in xml_entry}
+        self.work_type = work_type
 
-    SEARCH_URL = 'http://myanimelist.net/api/anime/search.xml'
+    @property
+    def poster(self) -> Optional[str]:
+        return self.raw_properties.get('image', None)
+
+    @property
+    def english_title(self) -> Optional[str]:
+        return self.raw_properties.get('english', None)
+
+    @property
+    def title(self) -> str:
+        return self.raw_properties.get('title', None)
+
+    @property
+    def start_date(self) -> Optional[str]:
+        xml_date = self.raw_properties.get('start_date', None)
+        if not xml_date or '0000' in xml_date:
+            return None
+
+        if '-00-00' in xml_date:
+            return xml_date.replace('-00-00', '-01-01')
+
+        if '-00' in xml_date:
+            return xml_date.replace('-00', '-01')
+
+        return xml_date
+
+    @property
+    def source_url(self) -> str:
+        return (
+            'https://myanimelist.net/{}/{}'.format(self.work_type.value, self.raw_properties['id'])
+        )
+
+
+class MALClient:
+    SEARCH_URL = 'https://myanimelist.net/api/{type}/search.xml'
     HEADERS = {
-        'X-Real-IP': random_ip(),
         'User-Agent': settings.MAL_USER_AGENT
     }
 
-    r = requests.get(SEARCH_URL, params={'q': query}, headers=HEADERS,
-                     auth=(settings.MAL_USER, settings.MAL_PASS))
-    html_code = html.unescape(re.sub(r'&amp;([A-Za-z]+);', r'&\1;', r.text))
-    xml = re.sub(r'&([^alg])', r'&amp;\1', _encoding_translation(html_code))
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers = self.HEADERS
+        self.session.auth = (settings.MAL_USER, settings.MAL_PASS)
 
-    entries = []
-    try:
+    @staticmethod
+    def _translate_http_exceptions(resp: requests.Response) -> None:
+        if resp.status_code == requests.codes['FORBIDDEN']:
+            raise RuntimeError('Invalid MAL credentials!')
+
+        if not resp.status_code == requests.codes['ALL_GOOD']:
+            raise RuntimeError('MAL request failure!')
+
+    def list_works_from_a_user(self, work_type: MALWorks, username: str) -> str:
+        resp = (
+            self.session.get(
+                'https://myanimelist.net/malappinfo.php?u={}&status=all&type={}'.format(username, work_type.value)
+            )
+        )
+
+        return resp.text
+
+    def _search_api(self, work_type: MALWorks, query: str) -> requests.Response:
+        return self.session.get(
+            self.SEARCH_URL.format(type=work_type.value),
+            params={
+                'q': query
+            })
+
+    def search_work(self, work_type: MALWorks, query: str) -> MALEntry:
+        resp = self._search_api(work_type, query)
+        self._translate_http_exceptions(resp)
+
+        html_code = html.unescape(re.sub(r'&amp;([A-Za-z]+);', r'a\1;', resp.text))
+        xml = re.sub(r'&([^alg])', r'&amp;\1', _encoding_translation(html_code))
+
+        return MALEntry(ET.fromstring(xml).find('entry'), work_type)
+
+    def search_works(self, work_type: MALWorks, query: str) -> List[MALEntry]:
+        resp = self._search_api(work_type, query)
+        self._translate_http_exceptions(resp)
+
+        html_code = html.unescape(re.sub(r'&amp;([A-Za-z]+);', r'a\1;', resp.text))
+        xml = re.sub(r'&([^alg])', r'&amp;\1', _encoding_translation(html_code))
+
+        entries = []
         for entry in ET.fromstring(xml).findall('entry'):
-            data = {}
-            for child in entry:
-                data[child.tag] = child.text
-            entries.append(data)
-    except ET.ParseError:
-        print(HEADERS)
-        pass
+            try:
+                mal_entry = MALEntry(entry, work_type)
+                entries.append(mal_entry)
+            except ET.ParseError:
+                logger.exception('MAL parsing error during search')
 
-    return entries
+            return entries
 
 
-def poster_url(query):
+client = MALClient()
+
+
+def insert_into_mangaki_database_from_mal(mal_entries: List[MALEntry]):
+    # Assumption: Mangaki's slugs are the same as MAL's work types.
+    if not mal_entries:
+        return
+
+    # All entries are supposed of the same type.
+    work_cat = Category.objects.get(slug=mal_entries[0].work_type.value)
+    for entry in mal_entries:
+        is_present = Work.objects.filter(
+            category=work_cat,
+            ext_poster=entry.poster
+        ).exists()
+
+        if not is_present:
+            Work.objects.create(
+                category=work_cat,
+                title=entry.english_title or entry.title,
+                source=entry.source_url,
+                ext_poster=entry.poster,
+                date=entry.start_date
+            )
+
+
+def poster_url(work_type: MALWorks, query: str) -> str:
     """
     Look for an anime on MyAnimeList and return the URL of its poster.
     """
 
-    SEARCH_URL = 'http://myanimelist.net/api/anime/search.xml'
-    HEADERS = {
-        'X-Real-IP': random_ip(),
-        'User-Agent': 'Mozilla/5.0 (X11; Linux i686 on x86_64; rv:36.0) Gecko/20100101 Firefox/36.0'
-    }
-
-    r = requests.get(SEARCH_URL,
-                     params={'q': query},
-                     headers=HEADERS,
-                     auth=(settings.MAL_USER, settings.MAL_PASS))
-
-    html_code = html.unescape(re.sub(r'&amp;([A-Za-z]+);', r'&\1;', r.text))
-    xml = re.sub(r'&([^alg])', r'&amp;\1', _encoding_translation(html_code))
-    return ET.fromstring(xml).find('entry').find('image').text
+    return client.search_work(work_type, query).poster
 
 
-def import_mal(mal_username, mangaki_username):
+def compute_rating_choice_from_mal_score(score: int) -> Optional[str]:
+    """
+    Compute from the MAL score, "the Mangaki choice" (like, neutral, favorite, dislike).
+    :param score: an integer between 0 and 10
+    :type score: int
+    :return: None if the score is out of the range, otherwise, [7, 10] => like, [5, 6] => neutral, [0, 4] => dislike.
+    :rtype: Optional[str]
+    """
+    if 7 <= score <= 10:
+        return 'like'
+    elif 5 <= score <= 6:
+        return 'neutral'
+    elif score >= 0:
+        return 'dislike'
+    else:
+        return None
+
+
+def get_or_create_from_mal(work_list: QuerySet,
+                           work_type: MALWorks,
+                           title: str,
+                           poster_link: str) -> Optional[Work]:
+    """
+    Get a work from the current `work_list` (a queryset, filtered by category)
+    or create from scratch using MAL as reference.
+    
+    :param work_list: a QuerySet of works
+    :type work_list: QuerySet<Work>
+    :param work_type: type of the work considered (e.g. MALWorks.animes)
+    :type work_type: MALWorks enum
+    :param title: title of the work
+    :type title: str
+    :param poster_link: poster URL of the work
+    :type poster_link: str
+    :return: a new work or already existing work!
+    :rtype: Work
+    """
+    try:
+        return work_list.get(title__iexact=title)
+    except Work.MultipleObjectsReturned:
+        works = work_list.filter(title__iexact=title).values_list('id').all()
+        logger.warning('Duplicates detected (title) for the work <{}> -- selecting first.'.format(title))
+        return works[0]
+    except Work.DoesNotExist:
+        count = work_list.filter(ext_poster=poster_link).count()
+        if count == 1:
+            return Work.objects.get(ext_poster=poster_link)
+        elif count >= 2:
+            logger.warning('Duplicates detected (ext_poster) for work with <{}> (during <{}> import) '
+                           '-- selecting first.'
+                           .format(poster_link, title))
+            return Work.objects.filter(ext_poster=poster_link).first()
+        else:
+            works = client.search_works(work_type, title)
+            insert_into_mangaki_database_from_mal(works)
+            return work_list.filter(
+                Q(ext_poster=poster_link)
+                | Q(title__iexact=title)).first()
+
+
+def import_mal(mal_username: str, mangaki_username: str):
     """
     Import myAnimeList by username
     """
 
-    MAL_URL = 'http://myanimelist.net/malappinfo.php?u=%s&status=all&type=anime' % mal_username
-    HEADERS = {
-        'X-Real-IP': random_ip(),
-        'User-Agent': settings.MAL_USER_AGENT
-    }
-    r = requests.get(MAL_URL, headers=HEADERS)
-    xml = ET.fromstring(r.text)
     user = User.objects.get(username=mangaki_username)
     nb_added = 0
     fails = []
-    for entry in xml:
-        if entry.tag == 'anime':
-            title = entry.find('series_title').text
-            poster = entry.find('series_image').text
-            score = int(entry.find('my_score').text)
-            mal_id = entry.find('series_animedb_id').text
+    mangaki_lists = {
+        MALWorks.animes: Work.objects.filter(category__slug='anime'),
+        MALWorks.mangas: Work.objects.filter(category__slug='manga')
+    }
+
+    for work_type in MALWorks:
+        xml = ET.fromstring(client.list_works_from_a_user(work_type, mal_username))
+        for entry in xml:
+            title = None
+            # Skip the myinfo part.
+            if entry.tag == 'myinfo':
+                continue
             try:
-                animes = Work.objects.filter(category__slug='anime')
-                try:
-                    anime = animes.get(title=title)
-                except Work.DoesNotExist:
-                    if animes.filter(ext_poster=poster).count() == 1:
-                        anime = Work.objects.get(ext_poster=poster)
-                    elif animes.filter(ext_poster=poster).count() >= 2:
-                        raise Exception('Integrity violation: found two or more works with the same poster, do you come from the past?')
-                    else:
-                        entries = lookup_mal_api(title)
-                        retrieve_anime(entries)
-                        anime = animes.get(ext_poster=poster)
-                if anime:
-                    if not Rating.objects.filter(user=user, work=anime).count():
-                        if 7 <= score <= 10:
-                            choice = 'like'
-                        elif 5 <= score <= 6:
-                            choice = 'neutral'
-                        elif score > 0:
-                            choice = 'dislike'
-                        else:
-                            continue
-                        Rating(user=user, work=anime, choice=choice).save()
-                        nb_added += 1
-            except Exception as e:
-                print(e)
+                title = entry.find('series_title').text
+                poster = entry.find('series_image').text
+                score = int(entry.find('my_score').text)
+                mal_id = entry.find('series_{}db_id'.format(work_type.value)).text
+            except AttributeError:
+                logger.exception('Malformed XML response (or MAL changed its API!)')
+                fails.append(title or 'Unknown failure')
+                continue
+
+            try:
+                work = get_or_create_from_mal(mangaki_lists[work_type], work_type, title, poster)
+
+                if work:
+                    already_rated = Rating.objects.filter(user=user, work=work).exists()
+                    if not already_rated:
+                        choice = compute_rating_choice_from_mal_score(score)
+                        if not choice:
+                            raise RuntimeError('No choice was deduced from MAL score.')
+
+                        Rating(user=user, choice=choice, work=work).save()
+
+                    nb_added += 1
+            except Exception:
+                logger.exception('Failure to fetch the work from MAL and import it into the Mangaki database.')
                 SearchIssue(user=user, title=title, poster=poster, mal_id=mal_id, score=score).save()
                 fails.append(title)
+
     return nb_added, fails
-
-class MAL:
-    def __init__(self):
-        self.SEARCH_URL = 'http://myanimelist.net/api/anime/search.xml'
-        self.HEADERS = {
-            'X-Real-IP': random_ip(),
-            'User-Agent': 'Mozilla/5.0 (X11; Linux i686 on x86_64; rv:36.0) Gecko/20100101 Firefox/36.0'
-        }
-        self.entry = None
-
-    def search(self, query):
-        r = requests.get(self.SEARCH_URL,
-            params={'q': query},
-            headers=self.HEADERS,
-            auth=(settings.MAL_USER, settings.MAL_PASS))
-        html_code = html.unescape(re.sub(r'&amp;([A-Za-z]+);', r'&\1;', r.text))
-        xml = re.sub(r'&([^alg])', r'&amp;\1', _encoding_translation(html_code))
-        try:
-            self.entry = ET.fromstring(xml).find('entry')
-        except ET.ParseError as e:
-            print(e)
-
-    def get_poster(self):
-        if self.entry:
-            return self.entry.find('image').text
-        else:
-            return ''
