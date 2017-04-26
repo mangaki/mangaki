1adf65155ff718aff29e706bb6aeae3f91e1a3fb
Jill-Jênn Vie <vie@jill-jenn.net>
2017-04-26 15:52:04 +0900

    Update notebooks

--- mangaki/mangaki/utils/data.py
+++ mangaki/mangaki/utils/data.py
@@ -16,13 +16,14 @@ AnonymizedData = namedtuple('AnonymizedData', 'X y nb_users nb_works')
 
 
 class Dataset:
-    anonymized = None
-    encode_user = None
-    decode_user = None
-    encode_work = None
-    decode_work = None
-    interesting_works = None
     def __init__(self):
+        self.anonymized = None
+        self.titles = None
+        self.encode_user = None
+        self.decode_user = None
+        self.encode_work = None
+        self.decode_work = None
+        self.interesting_works = None
         self.datetime = datetime.now()
 
     def save(self, filename):
@@ -33,11 +34,32 @@ class Dataset:
         with open(os.path.join(settings.PICKLE_DIR, filename), 'rb') as f:
             backup = pickle.load(f)
         self.anonymized = backup.anonymized
+        self.titles = backup.titles
         self.encode_user = backup.encode_user
         self.decode_user = backup.decode_user
         self.encode_work = backup.encode_work
         self.decode_work = backup.decode_work
         self.interesting_works = backup.interesting_works
+
+    def save_csv(self, suffix=''):
+        ratings_path = os.path.join(settings.DATA_DIR, 'ratings{}.csv'.format(suffix))
+        works_path = os.path.join(settings.DATA_DIR, 'works{}.csv'.format(suffix))
+        if os.path.isfile(ratings_path) or os.path.isfile(works_path):
+            confirm = input('Already exists. Continue? [y/n] ')
+        if confirm:
+            with open(ratings_path, 'w', newline='') as csvfile:
+                data = csv.writer(csvfile, delimiter=',', quotechar='', quoting=csv.QUOTE_NONE)
+                for (encoded_user_id, encoded_work_id), rating in zip(self.anonymized.X, self.anonymized.y):
+                    data.writerow([encoded_user_id, encoded_work_id, rating])
+            with open(works_path, 'w', newline='') as csvfile:
+                data = csv.writer(csvfile, delimiter=',')
+                lines = []
+                for work_id, title in self.titles.items():
+                    if work_id in self.encode_work:
+                        lines.append([self.encode_work[work_id], title])
+                lines.sort()
+                for line in lines:
+                    data.writerow(line)
 
     def load_csv(self, filename, convert=float):
         with open(os.path.join(settings.DATA_DIR, filename)) as f:
@@ -50,7 +72,7 @@ class Dataset:
             nb_works=max(triplets[:, 1]) + 1
         )
 
-    def make_anonymous_data(self, triplets):
+    def make_anonymous_data(self, triplets, convert=lambda choice: rating_values[choice]):
         triplets = list(triplets)
         users = set()
         works = set()
@@ -73,14 +95,14 @@ class Dataset:
         decode_work = dict(zip(anonymous_w, works))
 
         interesting_works = set()
-        for work_id, _ in nb_ratings.most_common():  # work_id are sorted by decreasing number of ratings
+        for work_id, _ in nb_ratings.most_common():  # work_id values are sorted by decreasing number of ratings
             if nb_ratings[work_id] < RATED_BY_AT_LEAST:
                 break
             interesting_works.add(work_id)
 
         for user_id, work_id, rating in triplets:
             X.append((encode_user[user_id], encode_work[work_id]))
-            y.append(rating_values[rating])
+            y.append(convert(rating))
 
         self.anonymized = AnonymizedData(X=np.array(X), y=np.array(y), nb_users=len(users), nb_works=len(works))
         self.encode_user = encode_user
