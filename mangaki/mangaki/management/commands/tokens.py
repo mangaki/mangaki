dc014a45a5a36f00e1625aee7bde69af8b0752ee
Jill-Jênn Vie <jill-jenn.vie@riken.jp>
2017-05-24 16:59:55 +0900

    Send mails with tokens

--- mangaki/mangaki/management/commands/tokens.py
+++ mangaki/mangaki/management/commands/tokens.py
@@ -1,8 +1,16 @@
 from django.core.management.base import BaseCommand
+from django.core.mail import send_mail
 from django.conf import settings
 from mangaki.utils.tokens import compute_token
 from django.contrib.auth import get_user_model
 from django.db import connection
+from django.conf import settings
+import os
+import yaml
+from jinja2 import Template
+
+
+DEBUG_USERNAMES = ['jj', 'RaitoBezarius']
 
 
 class Command(BaseCommand):
@@ -16,9 +24,27 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         username = options.get('username')[0]
         salt = options.get('salt')[0]
-        if username == '*':
-            for user in get_user_model().objects.filter(profile__newsletter_ok=True):
-                if user.email:
-                    pass  # Send a mail
-        else:
+        newsletter = yaml.load(open(os.path.join(settings.DATA_DIR, 'newsletter.yaml')).read())
+        message = Template(newsletter['body'])
+
+        if username not in ['DEBUG', '*']:
             self.stdout.write(self.style.SUCCESS(compute_token(salt, username)))
+            return
+
+        if username == 'DEBUG':
+            queryset = get_user_model().objects.filter(profile__newsletter_ok=True, username__in=DEBUG_USERNAMES)
+        else:
+            queryset = get_user_model().objects.filter(profile__newsletter_ok=True)[:5]
+        nb_mails = queryset.count()
+        for rank, user in enumerate(queryset, start=1):
+            if user.email:
+                token = compute_token(salt, user.username)
+                ok = send_mail(
+                    newsletter['subject'],
+                    message.render(username=user.username, token=token),
+                    newsletter['from'], [user.email], fail_silently=False)
+                prefix = '[{} / {}]'.format(rank, nb_mails)
+                if ok:
+                    self.stdout.write(self.style.SUCCESS('{}: {} OK'.format(prefix, user.username)))
+                else:
+                    self.stdout.write(self.style.ERROR('{}: {} NOK'.format(prefix, user.username)))
