4ecf83f74d4f7e51557054bed0a974f21291b98f
Raito Bezarius <masterancpp@gmail.com>
2017-06-10 23:28:02 +0200

    recommendations: load all ratings if only and only we will fit the algorithm (SVD, ALS, WALS, …)

--- mangaki/mangaki/utils/recommendations.py
+++ mangaki/mangaki/utils/recommendations.py
@@ -87,18 +87,16 @@ def get_reco_algo(request, algo_name='knn', category='all'):
                 for work_id, choice in current_user_ratings(request).items()
                 if choice not in ('willsee', 'wontsee')
             ])
-    else:
-        # Every rating is useful
-        triplets = list(
-            Rating.objects.values_list('user_id', 'work_id', 'choice'))
 
-    chrono.save('get all %d interesting ratings' % len(triplets))
-
-    dataset = Dataset()
     try:
         algo = get_algo_backup(algo_name)
         dataset = get_dataset_backup(algo_name)
     except FileNotFoundError:
+        # Every rating is useful
+        triplets = list(
+            Rating.objects.values_list('user_id', 'work_id', 'choice'))
+
+        chrono.save('get all %d interesting ratings' % len(triplets))
         dataset, algo = fit_algo(algo_name, triplets)
 
     chrono.save('fit %s' % algo.get_shortname())
