e44dbc2f84d3d3b3a67aed18a4cf294f9f3f2474
Kévin Cocchi <kevin.cocchi@gmail.com>
2017-07-13 19:09:39 +0200

    Studio missing fixed in tests too

--- mangaki/mangaki/tests/test_anidb.py
+++ mangaki/mangaki/tests/test_anidb.py
@@ -24,7 +24,7 @@ class AniDBTest(TestCase):
         # FIXME: The defaults for editor and studio in Work requires those to
         # exist, or else foreign key constraints fail.
         Editor.objects.create(pk=1)
-        # Studio.objects.create(pk=1)
+        Studio.objects.create(pk=1)
         self.anidb = client
         self.search_fixture = self.read_fixture('search_sangatsu_no_lion.xml')
         self.anime_fixture = self.read_fixture('anidb/sangatsu_no_lion.xml')
