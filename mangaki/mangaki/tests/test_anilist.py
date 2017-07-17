1fe1c1704eaa039059c2f5b906f001f365135095
Kévin Cocchi <kevin.cocchi@gmail.com>
2017-07-17 22:12:12 +0200

    More tests about authentication

--- mangaki/mangaki/tests/test_anilist.py
+++ mangaki/mangaki/tests/test_anilist.py
@@ -17,6 +17,7 @@ class AniListTest(TestCase):
 
     def setUp(self):
         self.anilist = client
+        self.no_anilist = AniList()
         self.fake_auth_json = '{"access_token":"fake_token","token_type":"Bearer","expires_in":3600,"expires":946684800}'
 
     def test_to_python_datetime(self):
@@ -24,6 +25,10 @@ class AniListTest(TestCase):
         self.assertEqual(to_python_datetime('20171200'), datetime(2017, 12, 1, 0, 0))
         self.assertEqual(to_python_datetime('20170000'), datetime(2017, 1, 1, 0, 0))
         self.assertRaises(ValueError, to_python_datetime, '2017')
+
+    def test_missing_client(self):
+        self.assertRaises(RuntimeError, self.no_anilist._authenticate)
+        self.assertFalse(self.no_anilist._is_authenticated())
 
     @responses.activate
     def test_authentication(self):
@@ -34,6 +39,8 @@ class AniListTest(TestCase):
             status=200,
             content_type='application/json'
         )
+
+        self.assertFalse(self.anilist._is_authenticated())
 
         auth = self.anilist._authenticate()
         self.assertEqual(auth["access_token"], "fake_token")
