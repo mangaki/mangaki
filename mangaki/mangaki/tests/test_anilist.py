7574e0dc1973fe1154d3fe5ac7253178dbc61bc8
Kévin Cocchi <kevin.cocchi@gmail.com>
2017-07-17 18:38:14 +0200

    Added a test when retrieving a user's AniList lists

--- mangaki/mangaki/tests/test_anilist.py
+++ mangaki/mangaki/tests/test_anilist.py
@@ -1,13 +1,19 @@
 from urllib.parse import urljoin
+import os
 
 import responses
 from django.conf import settings
 from django.test import TestCase
 
-from mangaki.wrappers.anilist import client, AniList
+from mangaki.wrappers.anilist import client, AniList, AniListWorks
 
 
 class AniListTest(TestCase):
+    @staticmethod
+    def read_fixture(filename):
+        with open(os.path.join(settings.TEST_DATA_DIR, filename), 'r', encoding='utf-8') as f:
+            return f.read()
+
     def setUp(self):
         self.anilist = client
         self.fake_auth_json = '{"access_token":"fake_token","token_type":"Bearer","expires_in":3600,"expires":946684800}'
@@ -27,3 +33,32 @@ class AniListTest(TestCase):
         self.assertEqual(auth["token_type"], "Bearer")
         self.assertEqual(auth["expires_in"], 3600)
         self.assertEqual(auth["expires"], 946684800)
+
+    @responses.activate
+    def test_get_userlist(self):
+        responses.add(
+            responses.POST,
+            urljoin(AniList.BASE_URL, AniList.AUTH_PATH),
+            body=self.fake_auth_json,
+            status=200, content_type='application/json'
+        )
+        responses.add(
+            responses.GET,
+            urljoin(AniList.BASE_URL, "user/mrsalixor/animelist"),
+            body=self.read_fixture('anilist/mrsalixor_anilist_animelist.json'),
+            status=200, content_type='application/json'
+        )
+        responses.add(
+            responses.GET,
+            urljoin(AniList.BASE_URL, "user/mrsalixor/mangalist"),
+            body=self.read_fixture('anilist/mrsalixor_anilist_mangalist.json'),
+            status=200, content_type='application/json',
+        )
+
+        anime_list = self.anilist.get_user_list(AniListWorks.animes, 'mrsalixor')
+        animes = set(anime_list)
+        self.assertEqual(len(animes), 360)
+
+        manga_list = self.anilist.get_user_list(AniListWorks.mangas, 'mrsalixor')
+        mangas = set(manga_list)
+        self.assertEqual(len(mangas), 57)
