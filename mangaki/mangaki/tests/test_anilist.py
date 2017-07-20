0ddeebb0a38df7e96ff426045385c323cf53d710
Kévin Cocchi <kevin.cocchi@gmail.com>
2017-07-21 00:36:11 +0200

    Retrieve seasonal animes from AniList (#428)
    Now, AniList provides a method to retrieve seasonal animes.

--- mangaki/mangaki/tests/test_anilist.py
+++ mangaki/mangaki/tests/test_anilist.py
@@ -6,7 +6,7 @@ import responses
 from django.conf import settings
 from django.test import TestCase
 
-from mangaki.wrappers.anilist import to_python_datetime, client, AniList, AniListWorks
+from mangaki.wrappers.anilist import to_python_datetime, to_anime_season, AniList, AniListStatus, AniListWorks
 
 
 class AniListTest(TestCase):
@@ -25,7 +25,6 @@ class AniListTest(TestCase):
             content_type='application/json'
         )
 
-    @responses.activate
     def setUp(self):
         self.anilist = AniList('test_client', 'client_secret')
         self.no_anilist = AniList()
@@ -35,6 +34,12 @@ class AniListTest(TestCase):
         self.assertEqual(to_python_datetime('20171200'), datetime(2017, 12, 1, 0, 0))
         self.assertEqual(to_python_datetime('20170000'), datetime(2017, 1, 1, 0, 0))
         self.assertRaises(ValueError, to_python_datetime, '2017')
+
+    def test_to_anime_season(self):
+        self.assertEqual(to_anime_season(datetime(2017, 1, 1, 0, 0)), 'winter')
+        self.assertEqual(to_anime_season(datetime(2017, 4, 1, 0, 0)), 'spring')
+        self.assertEqual(to_anime_season(datetime(2017, 7, 1, 0, 0)), 'summer')
+        self.assertEqual(to_anime_season(datetime(2017, 10, 1, 0, 0)), 'fall')
 
     def test_missing_client(self):
         self.assertRaises(RuntimeError, self.no_anilist._authenticate)
@@ -53,21 +58,44 @@ class AniListTest(TestCase):
         self.assertEqual(auth["expires"], 946684800)
 
     @responses.activate
-    def test_get_userlist(self):
+    def test_get_seasonal_anime(self):
         self.add_fake_auth()
 
         responses.add(
             responses.GET,
-            urljoin(AniList.BASE_URL, "user/mrsalixor/animelist"),
-            body=self.read_fixture('anilist/mrsalixor_anilist_animelist.json'),
+            urljoin(AniList.BASE_URL, 'browse/anime'),
+            body=self.read_fixture('anilist/airing_summer_2017_trimmed.json'),
             status=200, content_type='application/json'
         )
-        responses.add(
-            responses.GET,
-            urljoin(AniList.BASE_URL, "user/mrsalixor/mangalist"),
-            body=self.read_fixture('anilist/mrsalixor_anilist_mangalist.json'),
-            status=200, content_type='application/json',
-        )
+
+        for anime in self.anilist.list_seasonal_animes(year=2017, season='summer'):
+            if anime.title == 'Made in Abyss':
+                self.assertEqual(anime.anilist_id, 97986)
+                self.assertEqual(anime.english_title, 'Made in Abyss')
+                self.assertEqual(anime.japanese_title, 'メイドインアビス')
+                self.assertEqual(anime.media_type, 'TV')
+                self.assertEqual(anime.start_date, datetime(2017, 7, 7))
+                self.assertIsNone(anime.end_date)
+                self.assertEqual(anime.synonyms, [])
+                self.assertEqual(anime.genres, ['Adventure', 'Fantasy', 'Sci-Fi'])
+                self.assertFalse(anime.is_nsfw)
+                self.assertEqual(anime.poster_url, 'https://cdn.anilist.co/img/dir/anime/reg/97986-ZL0DkAyNWyxG.jpg')
+                self.assertEqual(anime.nb_episodes, 13)
+                self.assertEqual(anime.status, AniListStatus.airing)
+                self.assertEqual(anime.tags[1], {'anilist_tag_id': 175, 'name': 'Robots', 'spoiler': False})
+                break
+
+    @responses.activate
+    def test_get_userlist(self):
+        self.add_fake_auth()
+
+        for work_type in AniListWorks:
+            responses.add(
+                responses.GET,
+                urljoin(AniList.BASE_URL, 'user/mrsalixor/{}list'.format(work_type.value)),
+                body=self.read_fixture('anilist/mrsalixor_anilist_{}list.json'.format(work_type.value)),
+                status=200, content_type='application/json'
+            )
 
         anime_list = self.anilist.get_user_list(AniListWorks.animes, 'mrsalixor')
         animes = set(anime_list)
--- mangaki/mangaki/wrappers/anilist.py
+++ mangaki/mangaki/wrappers/anilist.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from enum import Enum
-from typing import Dict, Optional, Generator
+from typing import Dict, List, Optional, Any, Generator
 from urllib.parse import urljoin
 import time
 
@@ -25,13 +25,139 @@ def to_python_datetime(date):
      ...
     ValueError: no valid date format found for 2015
     """
-    date = date.strip()
+    date = str(date).strip()
     for fmt in ('%Y%m%d', '%Y%m00', '%Y0000'):
         try:
             return datetime.strptime(date, fmt)
         except ValueError:
             pass
     raise ValueError('no valid date format found for {}'.format(date))
+
+def to_anime_season(date):
+    """
+    Return the season corresponding to a date
+    >>> to_anime_season(datetime.datetime(2017, 3, 3, 0, 0))
+    'winter'
+    """
+    if 1 <= date.month <= 3:
+        return 'winter'
+    elif 4 <= date.month <= 6:
+        return 'spring'
+    elif 7 <= date.month <= 9:
+        return 'summer'
+    else:
+        return 'fall'
+
+
+class AniListWorks(Enum):
+    animes = 'anime'
+    mangas = 'manga'
+
+
+class AniListStatus(Enum):
+    aired = 'finished airing'
+    airing = 'currently airing'
+    published = 'finished publishing'
+    publishing = 'publishing'
+    anime_coming = 'not yet aired'
+    manga_coming = 'not yet published'
+    cancelled = 'cancelled'
+
+
+class AniListEntry:
+    def __init__(self, anime_info, work_type: AniListWorks):
+        self.anime_info = anime_info
+        self.work_type = work_type
+
+        if self.anime_info['series_type'] != work_type.value:
+            raise ValueError('AniList data not from {}'.format(work_type.value))
+
+    @property
+    def anilist_id(self) -> int:
+        return self.anime_info['id']
+
+    @property
+    def title(self) -> str:
+        return self.anime_info['title_romaji']
+
+    @property
+    def english_title(self) -> str:
+        return self.anime_info['title_english']
+
+    @property
+    def japanese_title(self) -> str:
+        return self.anime_info['title_japanese']
+
+    @property
+    def media_type(self) -> str:
+        return self.anime_info['type']
+
+    @property
+    def start_date(self) -> Optional[datetime]:
+        if self.anime_info['start_date_fuzzy']:
+            return to_python_datetime(self.anime_info['start_date_fuzzy'])
+        return None
+
+    @property
+    def end_date(self) -> Optional[datetime]:
+        if self.anime_info['end_date_fuzzy']:
+            return to_python_datetime(self.anime_info['end_date_fuzzy'])
+        return None
+
+    @property
+    def synonyms(self) -> List[str]:
+        return list(filter(None, self.anime_info['synonyms']))
+
+    @property
+    def genres(self) -> List[str]:
+        return list(filter(None, self.anime_info['genres']))
+
+    @property
+    def is_nsfw(self) -> bool:
+        return self.anime_info['adult']
+
+    @property
+    def poster_url(self) -> str:
+        return self.anime_info['image_url_lge']
+
+    @property
+    def nb_episodes(self) -> Optional[int]:
+        if self.work_type == AniListWorks.animes:
+            return self.anime_info['total_episodes']
+        return None
+
+    @property
+    def nb_chapters(self) -> Optional[int]:
+        if self.work_type == AniListWorks.mangas:
+            return self.anime_info['total_chapters']
+        return None
+
+    @property
+    def status(self) -> Optional[AniListStatus]:
+        if self.work_type == AniListWorks.animes:
+            return AniListStatus(self.anime_info['airing_status'])
+        elif self.work_type == AniListWorks.mangas:
+            return AniListStatus(self.anime_info['publishing_status'])
+        else:
+            return None
+
+    @property
+    def tags(self) -> List[Dict[str, Any]]:
+        return [
+            {
+                'name': tag['name'],
+                'anilist_tag_id': tag['id'],
+                'spoiler': tag['spoiler'] or tag['series_spoiler']
+            } for tag in self.anime_info['tags']
+        ]
+
+    def __str__(self) -> str:
+        return '<AniListEntry {}#{} : {} - {}>'.format(
+            self.work_type.value,
+            self.anilist_id,
+            self.title,
+            self.status.value
+        )
 
 
 class AniListWorks(Enum):
@@ -123,6 +249,25 @@ class AniList:
         r = self._session.get(urljoin(self.BASE_URL, datapage.format(**params)), params=query_params)
         r.raise_for_status()
         return r.json()
+
+    def list_seasonal_animes(self,
+                             *,
+                             only_airing: Optional[bool] = True,
+                             year: Optional[int] = None,
+                             season: Optional[str] = None) -> Generator[AniListEntry, None, None]:
+        if not year or not season:
+            now = datetime.now()
+            year = now.year
+            season = to_anime_season(now)
+
+        query_params = {'year': year, 'season': season, 'full_page': 'true'}
+
+        if only_airing:
+            query_params.update({'status': AniListStatus.airing.value})
+
+        data = self._request('browse/anime', query_params=query_params)
+        for anime_info in data:
+            yield AniListEntry(anime_info, AniListWorks.animes)
 
     def get_user_list(self,
                       worktype: AniListWorks,
