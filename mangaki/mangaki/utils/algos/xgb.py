ed1ba6834f11c8421b32cc88eec2432515316179
Raito Bezarius <masterancpp@gmail.com>
2017-07-25 15:36:50 +0200

    XGB using ALS vectors

--- mangaki/mangaki/utils/algos/xgb.py
+++ mangaki/mangaki/utils/algos/xgb.py
@@ -5,6 +5,7 @@ from xgboost.sklearn import XGBRegressor
 
 import pickle
 
+from .als import MangakiALS
 from mangaki.utils.algos.recommendation_algorithm import RecommendationAlgorithm
 
 logger = logging.getLogger(__name__)
@@ -42,7 +43,7 @@ class MangakiXGB(RecommendationAlgorithm):
             objective,
             tree_booster
         )
-
+        self.als = None
         self.nb_iterations = nb_iterations
 
     def load(self, filename):
@@ -51,44 +52,54 @@ class MangakiXGB(RecommendationAlgorithm):
     def save(self, filename):
         pickle.dumps(self.model, filename)
 
-    def make_matrix(self, X, y):
-        matrix = np.zeros((self.nb_users, self.nb_works), dtype=np.float16)
-        means = np.zeros((self.nb_users,))
+    def build_features(self, X, y):
+        als = MangakiALS(20, LAMBDA=-0.1)
+        als.set_parameters(self.nb_users, self.nb_works)
+        if self.verbose:
+            logger.info('Fitting ALS.')
+        als.fit(X, y)
 
-        for (user, work), rating in zip(X, y):
-            matrix[user][work] = rating
+        self.als = als
 
-        for i in range(self.nb_users):
-            means[i] = np.sum(matrix[i]) / np.sum(matrix[i] != 0)
-            if np.isnan(means[i]):
-                means[i] = 0
-            matrix[i][matrix[i] != 0] -= means[i]
+        return self.build_features_through_als(X)
 
-        return matrix
+    def build_features_through_als(self, X):
+        return np.array([
+            # Note we pass a *tuple* here.
+            np.concatenate((
+                # self.als.U[user_id] * self.als.VT.T[work_id],
+                self.als.U[user_id],
+                self.als.VT.T[work_id]
+            )) for user_id, work_id in X
+        ])
 
     def fit(self, X, y):
         if self.verbose:
-            logger.info("Computing `matrix`: (%i × %i)" % (self.nb_users, self.nb_works))
+            # len(y) lines.
+            # 40 columns: The ALS used has 20 components, so 2 * 20.
+            logger.info("Computing the feature `matrix` ({} × {}) through ALS vectors."
+                        .format(len(y), 2*20))
 
-        matrix = self.make_matrix(X, y)
+        matrix = self.build_features(X, y)
 
-        self.chrono.save('fill and center matrix')
+        if self.verbose:
+            logger.info('Shape of `matrix` is: {}'.format(matrix.shape))
 
-        X_prescaled = X.copy()
-        X_prescaled = (X_prescaled - X.min()) / (X.max() - X.min())
-        y_prescaled = y.copy()
-        y_prescaled = (y_prescaled - y.min()) / (y.max() - y.min())
+        self.chrono.save('fit ALS and build features matrix')
 
         self.model.fit(
-            X_prescaled,
-            y_prescaled,
+            matrix,
+            y,
             verbose=self.verbose
         )
 
         self.chrono.save('fit and boost trees')
 
     def predict(self, X):
-        return self.model.predict(X)
+        if not self.als:
+            raise ValueError('ALS has not been fitted! Cannot build the features!')
+
+        return self.model.predict(self.build_features_through_als(X))
 
     def get_shortname(self):
         params = self.model.get_params()
