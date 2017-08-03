07867ac200f0aff6db1e9ca963464083b4e29c8b
Jill-Jênn Vie <jill-jenn.vie@riken.jp>
2017-08-03 21:13:13 +0900

    Factor BALSE and add all_errors to common

--- mangaki/mangaki/utils/balse.py
+++ mangaki/mangaki/utils/balse.py
@@ -1,6 +1,7 @@
 from mangaki.utils.common import RecommendationAlgorithm
 from mangaki.utils.algo import get_algo_backup, get_dataset_backup
 from mangaki.utils.als import MangakiALS
+from mangaki.utils.lasso import MangakiLASSO
 from scipy.sparse import coo_matrix, load_npz
 from sklearn.linear_model import LinearRegression
 from collections import defaultdict
@@ -29,40 +30,26 @@ class MangakiBALSE(RecommendationAlgorithm):
         self.T = T
 
     def fit(self, X, y):
-        self.load_tags()
+        #self.load_tags()
         self.als = MangakiALS(10)
         self.als.set_parameters(self.nb_users, self.nb_works)
         self.als.fit(X, y)
-        y_pred = self.als.predict(X)
-        row = X[:, 0]
-        col = X[:, 1]
-        data = y - y_pred
-        print(type(y))
-        print('dt', y.dtype)
-        print(row[:5])
-        M_res = coo_matrix((data, (row, col)), shape=(self.nb_users, self.nb_works))
-        print(M_res)
-        M_res = M_res.tocsr()
-        self.reg = defaultdict(lambda: LinearRegression())
-        user_ids = sorted(set(row))
-        for user_id in user_ids:
-            indices = M_res[user_id].indices
-            values = M_res[user_id].data
-            self.reg[user_id].fit(self.T[indices], values)
-            if user_id % 100 == 0:
-                print(user_id)
+        #y_pred = self.als.predict(X)
+        self.lasso = MangakiLASSO()
+        self.lasso.set_parameters(self.nb_users, self.nb_works)
+        self.lasso.fit(X, y)
 
     def predict(self, X):
         # return self.M[X[:, 0].astype(np.int64), X[:, 1].astype(np.int64)] + self.means[X[:, 0].astype(np.int64)]
-        y_pred = self.als.predict(X)
-        y_delta_res = []
-        for user_id, work_id in X:
-            if user_id not in self.reg:
-                print(user_id, 'not')
-                y_delta_res.append(0)
+        y_als = self.als.predict(X)
+        y_lasso = self.lasso.predict(X)
+        y_pred = []
+        for i, (user_id, work_id) in enumerate(X):
+            if self.lasso.nb_rated[work_id] < 5:
+                y_pred.append(y_lasso[i])
             else:
-                y_delta_res.append(relu(self.reg[user_id].predict(self.T[work_id].reshape(1, -1))[0]))
-        return y_pred + y_delta_res
+                y_pred.append(y_als[i])
+        return y_pred
 
     def get_shortname(self):
         return 'balse-%d' % self.NB_COMPONENTS
