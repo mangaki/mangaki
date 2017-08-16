60568f62566c074bc397350c7b08176b8ffb6a4c
Stolinger <chalumeau.thomas@outlook.com>
2017-08-16 13:55:45 +0200

    Output file now supported to retrieve clusters and scores

--- mangaki/mangaki/management/commands/dedupe.py
+++ mangaki/mangaki/management/commands/dedupe.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 import os
 import csv
-from dedupe import Dedupe, consoleLabel
+from dedupe import Dedupe, consoleLabel, canonicalize
 from mangaki.models import Work
 
 
@@ -56,9 +56,10 @@ class Command(BaseCommand):
             clustered_dupes = deduper.match(data, threshold)
 
             print('# duplicate sets', len(clustered_dupes))
+
             input_file = 'dedupe/mangas.csv'
-            with open(input_file, 'w') as f:
-                writer = csv.writer(f)
+            with open(input_file, 'w', newline='') as f:
+                writer = csv.writer(f, delimiter=",")
                 writer.writerow(['id', 'title', 'vo_title'])
                 for work_id in data:
                     title = data[work_id]['title']
@@ -68,6 +69,51 @@ class Command(BaseCommand):
                     if vo_title != None:
                         vo_title = vo_title.encode('utf-8')
                     writer.writerow([work_id, title, vo_title])
+            cluster_membership = {}
+            cluster_id = 0
+            for (cluster_id, cluster) in enumerate(clustered_dupes):
+                id_set, scores = cluster
+                cluster_d = [data[c] for c in id_set]
+                canonical_rep = canonicalize(cluster_d)
+                for record_id, score in zip(id_set, scores):
+                    cluster_membership[record_id] = {
+                        "cluster id" : cluster_id,
+                        "canonical representation" : canonical_rep,
+                        "confidence": score
+                    }
+
+            singleton_id = cluster_id + 1
+
+            with open(output_file, 'w', newline='') as f_output, open(input_file) as f_input:
+                writer = csv.writer(f_output, delimiter=",")
+                reader = csv.reader(f_input)
+
+                heading_row = next(reader)
+                heading_row.insert(0, 'confidence_score')
+                heading_row.insert(0, 'Cluster ID')
+                canonical_keys = canonical_rep.keys()
+                for key in canonical_keys:
+                    heading_row.append('canonical_' + key)
+
+                writer.writerow(heading_row)
+
+                for row in reader:
+                    row_id = int(row[0])
+                    if row_id in cluster_membership:
+                        cluster_id = cluster_membership[row_id]["cluster id"]
+                        canonical_rep = cluster_membership[row_id]["canonical representation"]
+                        row.insert(0, cluster_membership[row_id]['confidence'])
+                        row.insert(0, cluster_id)
+                        for key in canonical_keys:
+                            row.append(canonical_rep[key].encode('utf8'))
+                    else:
+                        row.insert(0, None)
+                        row.insert(0, singleton_id)
+                        singleton_id += 1
+                        for key in canonical_keys:
+                            row.append(None)
+                    writer.writerow(row)
+            print('output file written')
 
         elif category == 'animes':
             data = {}
@@ -108,9 +154,10 @@ class Command(BaseCommand):
             clustered_dupes = deduper.match(data, threshold)
 
             print('# duplicate sets', len(clustered_dupes))
+
             input_file = 'dedupe/animes.csv'
-            with open(input_file, 'w') as f:
-                writer = csv.writer(f)
+            with open(input_file, 'w', newline='') as f:
+                writer = csv.writer(f, delimiter=",")
                 writer.writerow(['id', 'title', 'vo_title'])
                 for work_id in data:
                     title = data[work_id]['title']
@@ -120,3 +167,50 @@ class Command(BaseCommand):
                     if vo_title != None:
                         vo_title = vo_title.encode('utf-8')
                     writer.writerow([work_id, title, vo_title])
+
+            cluster_membership = {}
+            cluster_id = 0
+            for (cluster_id, cluster) in enumerate(clustered_dupes):
+                id_set, scores = cluster
+                cluster_d = [data[c] for c in id_set]
+                canonical_rep = canonicalize(cluster_d)
+                for record_id, score in zip(id_set, scores):
+                    cluster_membership[record_id] = {
+                        "cluster id" : cluster_id,
+                        "canonical representation" : canonical_rep,
+                        "confidence": score
+                    }
+
+            singleton_id = cluster_id + 1
+
+            with open(output_file, 'w', newline='') as f_output, open(input_file) as f_input:
+                writer = csv.writer(f_output, delimiter=",")
+                reader = csv.reader(f_input)
+
+                heading_row = next(reader)
+                heading_row.insert(0, 'confidence_score')
+                heading_row.insert(0, 'Cluster ID')
+                canonical_keys = canonical_rep.keys()
+                for key in canonical_keys:
+                    heading_row.append('canonical_' + key)
+
+                writer.writerow(heading_row)
+
+                for row in reader:
+                    row_id = int(row[0])
+                    if row_id in cluster_membership:
+                        cluster_id = cluster_membership[row_id]["cluster id"]
+                        canonical_rep = cluster_membership[row_id]["canonical representation"]
+                        row.insert(0, cluster_membership[row_id]['confidence'])
+                        row.insert(0, cluster_id)
+                        for key in canonical_keys:
+                            row.append(canonical_rep[key].encode('utf8'))
+                    else:
+                        row.insert(0, None)
+                        row.insert(0, singleton_id)
+                        singleton_id += 1
+                        for key in canonical_keys:
+                            row.append(None)
+                    writer.writerow(row)
+            print('output file written')
+
