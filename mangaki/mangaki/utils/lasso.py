Fix LASSO by scaling features

mangaki/mangaki/utils/lasso.py
@@ -1,10 +1,11 @@
 from mangaki.utils.common import RecommendationAlgorithm
-#from mangaki.utils.algo import get_algo_backup, get_dataset_backup
-#from mangaki.utils.als import MangakiALS
+from mangaki.settings import DATA_DIR
 from scipy.sparse import coo_matrix, load_npz
-from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import scale
 from collections import Counter, defaultdict
-import numpy as np
+from mangaki.utils.stats import avgstd
+import os.path
 
 
 def relu(x):
@@ -12,18 +13,17 @@ def relu(x):
 
 
 class MangakiLASSO(RecommendationAlgorithm):
-    def __init__(self, NB_COMPONENTS=10, NB_ITERATIONS=10, LAMBDA=0.1):
+    def __init__(self, with_bias=True, alpha=0.01):
         super().__init__()
-        self.NB_COMPONENTS = NB_COMPONENTS
-        self.NB_ITERATIONS = NB_ITERATIONS
-        self.LAMBDA = LAMBDA
+        self.alpha = alpha
+        self.with_bias = with_bias
 
     def load_tags(self, T=None):
-        # From file
         if T is None:
-            T = load_npz('../data/balse/tag-matrix.npz').toarray()
+            T = load_npz(os.path.join(DATA_DIR, 'balse/tag-matrix.npz')).tocsc()
+            T_scaled = scale(T, with_mean=False).toarray()
         _, self.nb_tags = T.shape
-        self.T = T
+        self.T = T_scaled
 
     def fit(self, X, y):
         self.load_tags()
@@ -32,14 +32,15 @@ class MangakiLASSO(RecommendationAlgorithm):
         self.nb_rated = Counter(col)
         data = y
         M = coo_matrix((data, (row, col)), shape=(self.nb_users, self.nb_works)).tocsr()
-        self.reg = defaultdict(lambda: Lasso(alpha=0.001, normalize=True))
+        self.reg = defaultdict(lambda: Lasso(alpha=self.alpha, fit_intercept=self.with_bias))
         user_ids = sorted(set(row))
         for user_id in user_ids:
             indices = M[user_id].indices
             values = M[user_id].data
             self.reg[user_id].fit(self.T[indices], values)
-            if user_id % 100 == 0:
+            if user_id % 500 == 0:
                 print(user_id)
+        self.compute_sparsity()
 
     def predict(self, X):
         y_pred = []
@@ -51,5 +52,12 @@ class MangakiLASSO(RecommendationAlgorithm):
                 y_pred.append(relu(self.reg[user_id].predict(self.T[work_id].reshape(1, -1))[0]))
         return y_pred
 
+    def compute_sparsity(self):
+        sparsity = []
+        for user_id in self.reg:
+            nb_features = sum(weight != 0 for weight in self.reg[user_id].coef_)
+            sparsity.append(nb_features)
+        print('Sparsity', avgstd(sparsity))
+
     def get_shortname(self):
-        return 'balse-%d' % self.NB_COMPONENTS
+        return 'lasso-%d' % self.NB_COMPONENTS