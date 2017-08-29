ebc22b46678ae4e499dad9f120c07f87fd70978c
Raito Bezarius <masterancpp@gmail.com>
2017-08-29 16:50:54 +0200

    Round up to 6 decimals for avgstd

--- mangaki/mangaki/utils/stats.py
+++ mangaki/mangaki/utils/stats.py
@@ -1,7 +1,8 @@
 import math
 
+
 def avgstd(l):  # Displays mean and variance
     n = len(l)
     mean = float(sum(l)) / n
     var = float(sum(i * i for i in l)) / n - mean * mean
-    return '%.5f ± %.5f' % (round(mean, 5), round(1.96 * math.sqrt(var / n), 3))  # '%.3f ± %.3f' % 
+    return '%.5f ± %.5f' % (round(mean, 6), round(1.96 * math.sqrt(var / n), 6))
