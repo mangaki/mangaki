73e949e329d5ec4faf1d104798acddc3513ccd7a
Raito Bezarius <masterancpp@gmail.com>
2017-09-03 16:49:15 +0200

    Fancy formatting and move experiments into their own folder

--- mangaki/mangaki/management/commands/compare.py
+++ mangaki/mangaki/management/commands/compare.py
@@ -22,7 +22,9 @@ CONVERT_FUNCTIONS = {
     'movies': float,
     'mangas': lambda choice: rating_values[choice]
 }
-DEFAULT_CONFIG_FILENAME = os.path.join(DATA_DIR, 'experiment_config.json')
+
+EXPERIMENTS_FOLDER = os.path.join(DATA_DIR, 'experiments')
+DEFAULT_CONFIG_FILENAME = os.path.join(EXPERIMENTS_FOLDER, 'default.json')
 
 logger = logging.getLogger(__name__)
 
@@ -41,7 +43,8 @@ class AlgorithmWrapper:
 
 
 class Experiment(object):
-    def __init__(self, dataset_name, eval_metrics, experiment_filename=None):
+    def __init__(self, dataset_name, eval_metrics, experiment_filename=None,
+                 fancy_formatting: bool = False):
         self.algos = []
 
         self.experiment_filename = experiment_filename
@@ -50,6 +53,7 @@ class Experiment(object):
 
         self.evaluation_metrics = eval_metrics
         self.anonymized = None
+        self.fancy_formatting = fancy_formatting
         self.load_dataset(dataset_name)
 
     def prepare_experiment(self):
@@ -97,6 +101,28 @@ class Experiment(object):
 
         return results
 
+    def format_final_result(self, algo_name: str, results: List[float]):
+        # noinspection PyTypeChecker
+        mean = float(np.mean(results))
+        # noinspection PyTypeChecker
+        var = float(np.var(results))
+        std = 1.96 * np.sqrt(var / len(results))
+
+        if self.fancy_formatting:
+            return '[{}] {:5f} ± {:5f}'.format(
+                algo_name,
+                round(mean, 6),
+                round(std, 6)
+            )
+
+        else:
+            return '[{}] mean={:f} var={:f} std={:f}'.format(
+                algo_name,
+                mean,
+                var,
+                std
+            )
+
     def compare_models(self, nb_split: int, full_cv: bool):
         if not self.algos:
             logger.warning('No algorithms has been specified in this experiment. Stopping early!'
@@ -126,15 +152,14 @@ class Experiment(object):
         for metric_name, algos in metrics.items():
             logger.info('Evaluation of {}:'.format(metric_name.upper()))
             for algo_name in algos.keys():
-                mean = np.mean(algos[algo_name])
-                var = np.var(algos[algo_name])
-                std = var ** 0.5
-                logger.info('[{}]: mean={:f} var={:f} std={:f}'.format(
-                    algo_name,
-                    mean,
-                    var,
-                    std
-                ))
+                if full_cv and nb_split > 1:
+                    logger.info(self.format_final_result(algo_name,
+                                                         algos[algo_name]))
+                else:
+                    mean = np.mean(algos[algo_name])
+                    logger.info('[{}]: {:f}'.format(algo_name, mean))
+
+
 
 
 class Command(BaseCommand):
@@ -163,6 +188,12 @@ class Command(BaseCommand):
                             default=5,
                             help='How many splits should be done on the dataset using a sklearn ShuffleSplit '
                                  '(default: 5-fold)')
+        parser.add_argument('-fancy', '--fancy-formatting',
+                            dest='fancy_formatting',
+                            action='store_true',
+                            default=False,
+                            help='Fancy format the final results in the format (mean ± std) cut to 5 digits '
+                                 'after rounding to 6 digits.')
 
     def handle(self, *args, **options):
         verbosity_level = options.get('verbosity')
@@ -171,6 +202,7 @@ class Command(BaseCommand):
         eval_metrics = options.get('eval_metrics')
         experiment_filename = options.get('experiment_filename')
         nb_split = options.get('nb_split')
+        fancy_formatting = options.get('fancy_formatting')
 
         # We allow DEBUG to be the minimal level.
         # Refer to Logging Levels for more information, but basically, levels are in multiple of 10.
@@ -187,5 +219,6 @@ class Command(BaseCommand):
             logger.debug('Compare will perform only one run.')
         else:
             logger.debug('Compare will perform a full cross validation of {}-fold.'.format(nb_split))
-        experiment = Experiment(dataset_name, eval_metrics, experiment_filename)
+
+        experiment = Experiment(dataset_name, eval_metrics, experiment_filename, fancy_formatting)
         experiment.compare_models(nb_split, full_cv=full_cv)
