5ef92917d7e7471e830f3a9f1f2c0b33459cd560
Raito Bezarius <masterancpp@gmail.com>
2017-09-10 19:09:27 +0200

    Move into mangaki application directory and remove LOCALE_PATHS setting, add .mo in the package

--- mangaki/mangaki/settings.py
+++ mangaki/mangaki/settings.py
@@ -241,7 +241,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
