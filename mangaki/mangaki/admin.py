ea6af1507842bfe57f01597df7068060d193d487
Kévin Cocchi <kevin.cocchi@gmail.com>
2017-09-23 23:00:30 +0200

    Fix error 500 when searching TaggedWorks in admin (#485)

--- mangaki/mangaki/admin.py
+++ mangaki/mangaki/admin.py
@@ -559,7 +559,7 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(TaggedWork)
 class TaggedWorkAdmin(admin.ModelAdmin):
-    search_fields = ('work', 'tag')
+    search_fields = ('work__title', 'tag__title')
 
 
 @admin.register(WorkCluster)
