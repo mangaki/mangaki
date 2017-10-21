064da36aadcacc17c3c352d342fa32e021178bb6
Jill-Jênn Vie <vie@jill-jenn.net>
2017-10-21 19:25:39 +0900

    Test all recommendation algorithms in the factory

--- mangaki/mangaki/algo/__init__.py
+++ mangaki/mangaki/algo/__init__.py
@@ -7,7 +7,6 @@ from .balse import MangakiBALSE
 from .lasso import MangakiLASSO
 from .efa import MangakiEFA
 from .knn import MangakiKNN
-from .nmf import MangakiNMF
 from .pca import MangakiPCA
 from .svd import MangakiSVD
 from .wals import MangakiWALS
---
+++ mangaki/mangaki/tests/test_algo.py
@@ -0,0 +1,33 @@
+import logging
+import numpy as np
+
+from mangaki.algo.recommendation_algorithm import RecommendationAlgorithm
+from django.test import TestCase
+
+
+class AlgoTest(TestCase):
+    def setUp(self):
+        self.nb_users = 5
+        self.nb_works = 10
+        self.U = np.random.random((self.nb_users, 2))
+        self.VT = np.random.random((2, self.nb_works))
+        self.M = self.U.dot(self.VT)
+        train_user_ids = [1, 2, 3, 3]
+        train_work_ids = [0, 1, 0, 1]
+        self.X_train = np.column_stack((train_user_ids, train_work_ids))
+        self.y_train = self.M[train_user_ids, train_work_ids]
+        test_user_ids = [1, 2]
+        test_work_ids = [1, 0]
+        self.X_test = np.column_stack((test_user_ids, test_work_ids))
+        self.y_test = self.M[test_user_ids, test_work_ids]
+
+    def test_missing_client(self):
+        for algo_name in RecommendationAlgorithm.list_available_algorithms():
+            if algo_name == 'wals':
+                continue
+            algo = RecommendationAlgorithm.instantiate_algorithm(algo_name)
+            algo.set_parameters(self.nb_users, self.nb_works)
+            algo.fit(self.X_train, self.y_train)
+            y_pred = algo.predict(self.X_test)
+            logging.debug('rmse=%.3f algo=%s',
+                          algo.compute_rmse(y_pred, self.y_test), algo_name)
