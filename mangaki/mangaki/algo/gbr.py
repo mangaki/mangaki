f4e334241d1ccb63bd226906732d82b65f36aa11
Jill-Jênn Vie <vie@jill-jenn.net>
2017-11-13 18:19:05 +0900

    Handle side information and cleanup GBR

--- mangaki/mangaki/algo/gbr.py
+++ mangaki/mangaki/algo/gbr.py
@@ -6,77 +6,59 @@ from django.conf import settings
 import json
 import numpy as np
 import os.path
-
-
-I2V_DIR = os.path.join(settings.DATA_DIR, 'illustration2vec')
+import logging
 
 
 @register_algorithm('gbr')
 class MangakiGBR(RecommendationAlgorithm):
-	U = None
-	M = None
-	dataset = Dataset()
-	def __init__(self, NB_COMPONENTS=10, NB_ITERATIONS=10, LAMBDA=0.1):
-		super().__init__()
-		self.NB_COMPONENTS = NB_COMPONENTS
-		self.NB_ITERATIONS = NB_ITERATIONS
-		self.LAMBDA = LAMBDA
+    def __init__(self, nb_components=20, nb_estimators=2):
+        super().__init__()
+        self.nb_components = nb_components
+        self.nb_estimators = nb_estimators
+        self.T = None
 
-	def load(self, filename):
-		backup = super().load(filename)
-		self.M = backup.M
-		self.U = backup.U
-		self.dataset.load('rating' + self.get_backup_filename())
+    def load(self, filename):
+        backup = super().load(filename)
+        # What should be saved for this model? Depends on gbr.get_params
 
-	def fit(self, X, y):
-		als_algo = MangakiALS(20)
-		als_algo.set_parameters(self.nb_users,self.nb_works)
-		als_algo.fit(X, y)
-		tags = json.load(open(os.path.join(I2V_DIR, 'mangaki_i2v.json')))
-		self.chrono.save("fitting als algo")
-		V = np.transpose(als_algo.VT)
+    def prepare_features(self, X, U, V):
+        X_full = []
+        for (user_id, work_id) in X:
+            features = np.concatenate((U[user_id],
+                                       V[work_id],
+                                       #  self.T[work_id], #  Possible but 98s
+                                       U[user_id] * V[work_id]))
+            X_full.append(features)
+        return np.array(X_full)
 
-		try:
-			tags_definition = json.load(open(os.path.join(I2V_DIR, 'all_tags.json')))
-		except FileNotFoundError:
-			tags = json.load(open(os.path.join(I2V_DIR, 'mangaki_i2v.json')))
-			all_tags = set()
-			for poster in tags:
-				all_tags.update([tag for tag, _ in tags[poster]['general']])
-			tags_definition = {}
-			i = 0
-			for tag in all_tags:
-				tags_definition[tag] = i
-				i += 1
+    def fit(self, X, y):
+        if self.T is None:
+            self.load_tags()
 
-			json.dump(tags_definition, open(os.path.join(I2V_DIR, 'all_tags.json'), 'w'))
+        self.als = MangakiALS(self.nb_components)
+        try:
+            self.als.load(self.als.get_backup_filename())
+        except:
+            self.als.set_parameters(self.nb_users, self.nb_works)
+            self.als.fit(X, y)
+            self.als.compute_all_errors(X, y, X, y)
 
-		nb_tags = len(tags_definition)
-		poster_tags = np.zeros((self.nb_works, nb_tags))
-		for i in range(self.nb_works):
-			poster = str(self.dataset.decode_work[i])+'.jpg'
-			if poster in tags:
-				for tag, value in tags[poster]['general']:
-					poster_tags[i][tags_definition[tag]] = value
-		i = 0
-		for (user_id, work_id) in X:
-			self.U = np.zeros((len(y), als_algo.U.shape[1]+V.shape[1]+nb_tags))
-			iline = []
-			iline.extend(als_algo.U[user_id])
-			iline.extend(V[work_id])
-			iline.extend(poster_tags[work_id])
-			self.U[i] = iline
-		self.M = GBR()
-		self.M.fit(self.U,y)
+        self.chrono.save('fit ALS model')
 
-	def predict(self,X):
-		X_selected = set()
-		for user_id in X:
-			X_selected.update([user_id[0]])
-		return self.M.predict(self.U[list(X_selected)])
+        X_full = self.prepare_features(X, self.als.U, self.als.VT.T)
 
-	def load_dataset(self, to_load):
-		self.dataset = to_load
+        self.chrono.save('build features')
 
-	def get_shortname(self):
-		return 'gbr-%d' % self.NB_COMPONENTS
+        self.gbr = GBR(n_estimators=self.nb_estimators)
+        self.gbr.fit(X_full, y)
+        logging.info(self.gbr.feature_importances_)
+        logging.info(self.gbr.train_score_)
+
+        self.chrono.save('fit GBR model')
+
+    def predict(self, X):
+        X_full = self.prepare_features(X, self.als.U, self.als.VT.T)
+        return self.gbr.predict(X_full)
+
+    def get_shortname(self):
+        return 'gbr-%d' % self.nb_components
