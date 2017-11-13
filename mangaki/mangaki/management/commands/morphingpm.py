7e7040b7edd0742c872f1777c0aabef6f7aa896b
Jill-Jênn Vie <vie@jill-jenn.net>
2017-11-13 20:20:36 +0900

    Make code executable

--- mangaki/mangaki/management/commands/morphingpm.py
+++ mangaki/mangaki/management/commands/morphingpm.py
@@ -2,24 +2,12 @@ from django.core.management.base import BaseCommand, CommandError
 
 import os
 import json
-from mangaki.utils.values import tags_definition
 from math import floor
 import numpy as np
 from mangaki.settings import DATA_DIR
+from mangaki.algo.lasso import load_and_scale_tags
 
-posters = json.load(open(os.path.join(DATA_DIR, 'mangaki_i2v.json')))
-nb_tags = len(tags_definition)
-index_posters = [.0] * len(posters)
-i = 0
-for poster in posters:
-	index = int(poster[:len(poster)-4])
-	index_posters[i] = index
-	i += 1
-poster_tags = np.zeros((max(index_posters)+1, nb_tags))
-for poster in posters:
-	index = int(poster[:len(poster)-4])
-	for tag, value in posters[poster]['general']:
-		poster_tags[index][tags_definition[tag]] = value
+nb_tags, poster_tags = load_and_scale_tags()
 
 def distance(a, b):
 	dist = (b - a)
@@ -38,7 +26,7 @@ def morphing(a, b, subdiv = 4):
 	nb_posters = poster_tags.shape[0]
 	for i in range (nb_posters):
 		# if the selected poster is in fact a poster and is neither the goal or the beginning
-		if i!=a and i!=b and i in index_posters:
+		if i!=a and i!=b:
 			current = poster_tags[i] - poster_tags[a]
 			projection = np.dot(current, segment)/seg_norm
 			# if the projection is contained in the segment [a,b] with a subdiv/2 offset for more diverse morphing
--- mangaki/mangaki/utils/values.py
+++ mangaki/mangaki/utils/values.py
@@ -1,22 +1,3 @@
-import json
-import os
-from mangaki.settings import DATA_DIR
-
 rating_values = {'favorite': 4, 'like': 2, 'dislike': -2, 'neutral': 0.1, 'willsee': 0.5, 'wontsee': -0.5}
 rating_values_dpp = {'like': 2, 'dislike': -2, 'dontknow': 0}
 # rating_values = {'favorite': 1, 'like': 1, 'neutral': 0, 'dislike': 0, 'willsee': 0, 'wontsee': 0}  # For NMF, do not use in production!
-
-try:
-	tags_definition = json.load(open(os.path.join(DATA_DIR, 'all_tags.json')))
-except FileNotFoundError:
-	tags = json.load(open(os.path.join(DATA_DIR, 'mangaki_i2v.json')))
-	all_tags = set()
-	for poster in tags:
-		all_tags.update([tag for tag, _ in tags[poster]['general']])
-	tags_definition = {}
-	i = 0
-	for tag in all_tags:
-		tags_definition[tag] = i
-		i += 1
-
-	json.dump(tags_definition, open(os.path.join(DATA_DIR, 'all_tags.json'), 'w'))
