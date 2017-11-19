1da7cebb8d09a6cdd849953e025b8be840a6eddd
Ryan Lahfa <masterancpp@gmail.com>
2017-11-19 05:49:49 +0100

    Auto-deployment for master branch (#507)
    * Fix Celery / Redis on Vagrant
    
    * Repair Redis deployment on beta.mangaki.fr
    
    * Auto-deployment!
    
    With Ansible :-].
    
    * Let's take a existing version of Ansible.
    
    * Fix SSH connection for CircleCI
    
    * Use `.items()` for cron.service.j2 rather than `.iteritems()`
    Use latest Ansible for Python 2 rather than Python 3.
    
    * Fix line that selects the latest sdist
    
    * Ignore ResponseError such as NOAUTH during Redis Pool creation

---
+++ provisioning/scripts/deploy_beta.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Assumptions:
+# - You're currently in somewhere/mangaki_repository/
+# - You're already under some virtual env which can execute setup.py sdist.
+
+SITE_YAML="provisioning/site.yml"
+BETA_INVENTORY_HOSTS="provisioning/inventories/staging/hosts"
+BETA_PASSWORD_FILE_PATH="$(pwd)/.vault_pass.txt"
+
+# Sanity checks.
+if [ -z "$BETA_VAULT_PASSWORD" ]
+then
+        echo "BETA_VAULT_PASSWORD environment variable is not set. Cannot continue."
+        exit 1
+fi
+
+# Get a hang of where are we.
+# Prepare a nice package.
+cd mangaki && python setup.py sdist && cd ..
+# Get the path to the package.
+LOCAL_PACKAGE_NAME=$(find "mangaki/dist" -maxdepth 1 -name "*.tar.gz" | xargs ls -t | head -n1)
+LOCAL_PACKAGE_PATH="$(pwd)/$LOCAL_PACKAGE_NAME"
+echo "I will install $LOCAL_PACKAGE_PATH on beta.mangaki.fr."
+# Prepare the password file.
+touch ~/.vault_pass.txt
+chmod 600 ~/.vault_pass.txt
+# Put the password in a file for Ansible Vault.
+echo "$BETA_VAULT_PASSWORD" > "$BETA_PASSWORD_FILE_PATH"
+echo "Password inserted."
+# Run the playbook.
+ansible-playbook $SITE_YAML \
+        -e mangaki_sync_compilemessages=true \
+        -e mangaki_sync_collectstatic=true \
+        -e mangaki_sync_migrate=true \
+        -e mangaki_source_package_local="$LOCAL_PACKAGE_PATH" \
+        -i "$BETA_INVENTORY_HOSTS" \
+        --vault-password-file "$BETA_PASSWORD_FILE_PATH"
+ANSIBLE_STATUS=$?
+
+# Clean up the evidence.
+rm "$BETA_PASSWORD_FILE_PATH"
+echo "Password deleted."
+
+# Inform the CI in case of problems.
+if [ $ANSIBLE_STATUS -ne 0 ]
+then
+        echo "Failed to provision beta. Failing the test."
+        exit 1
+fi
