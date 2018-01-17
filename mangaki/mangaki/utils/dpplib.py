5d07fb3928c24a188a75be0f15d927128a066051
Jill-Jênn Vie <vie@jill-jenn.net>
2018-01-17 20:51:55 +0900

    Test DPP

--- mangaki/mangaki/utils/dpplib.py
+++ mangaki/mangaki/utils/dpplib.py
@@ -1,15 +1,6 @@
 import numpy as np
 
 
-def decompose_kernel(L):
-  D, V = np.linalg.eigh(L)
-  D = np.real(D)
-  D[D<0] = 0
-  idx = np.argsort(D)
-  D = D[idx]
-  V = np.real(V[:, idx])
-  return D, V
-
 def esym_poly(k, lam):
   N = lam.size
   E = np.zeros((k+1, N+1))
@@ -19,60 +10,6 @@ def esym_poly(k, lam):
       E[l, n] = E[l, n-1] + lam[n-1]*E[l-1, n-1]
 
   return E
-
-def E_Y(lam):
-  return np.sum(lam/(1+lam))
-
-def modifyLam(lam, c):
-  return c*lam/(1+lam*(1-c))
-
-def analyze_bDPP(lam):
-  print("Ground set size:\t{0}".format(len(lam)))
-  d = E_Y(lam)
-  print("E[|Y|]:\t\t\t{0}".format(d))
-  print("Maximum eigenvalue:\t{0}".format(np.max(lam)+1))
-  cmax = 1/(np.max(lam)) + 1
-  print("Maximum k:\t\t{0}".format(cmax*d))
-
-def sample_EY(lam, V_full, k):
-  d = E_Y(lam)
-  cmax = 1/(np.max(lam)) + 1
-  c = k/d
-  if c>cmax:
-    print("Warning: c>cmax. Continuing with regular DPP.")
-    return sample(lam, V_full)
-
-  L_C = modifyLam(lam, c)
-  return sample(L_C, V_full)
-
-def sample(lam, V_full):
-  D = lam/(1+lam)
-  v = np.random.rand(1, len(lam)) <= D
-  k = np.sum(v)
-  V = V_full[:, v.flatten()]
-  Y = np.zeros((k, 1))
-
-  for i in range(k-1, -1, -1):
-    # Sample
-    Pr = np.sum(V**2, axis=1)
-    Pr = Pr/sum(Pr)
-    C = np.cumsum(Pr)
-    jj = np.argwhere(np.random.rand() <= C)[0]
-    Y[i] = jj
-
-    # Update V 
-    if i > 0:
-      j = np.argwhere(V[int(Y[i]), :])[0]
-      Vj = V[:, j]
-      V = np.delete(V, j, 1)
-      V = V - np.outer(Vj, V[int(Y[i]), :]/Vj[int(Y[i])])
-
-      # QR decomposition, which is more numerically stable (and faster) than Gram
-      # Schmidt
-      V, r = np.linalg.qr(V)
-
-  return Y
-
 
 
 def sample_k(k, lam, V_full):
@@ -121,4 +58,3 @@ def sample_k(k, lam, V_full):
       V, r = np.linalg.qr(V)
 
   return Y
-
