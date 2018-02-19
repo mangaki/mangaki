0551e37a6c6eb67e365cabc24f5cd038f59b5f01
Jill-Jênn Vie <vie@jill-jenn.net>
2018-02-19 22:25:53 +0900

    Add new recommendation algorithm FMA

--- mangaki/mangaki/algo/__init__.py
+++ mangaki/mangaki/algo/__init__.py
@@ -7,6 +7,7 @@ from .balse import MangakiBALSE
 from .lasso import MangakiLASSO
 from .cfm import MangakiCFM
 from .efa import MangakiEFA
+from .fma import MangakiFMA
 from .gbr import MangakiGBR
 from .knn import MangakiKNN
 from .knn2 import MangakiKNN2
