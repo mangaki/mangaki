ee4be3acad7221a2faca1816c995b3769641608d
Edmund Wu <fangkazuto@gmail.com>
2020-09-11 13:14:59 -0400

    nix: include flake
    Merge nixpkgs-overlays into the flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Mangaki";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs = { type = "github"; owner = "NixOS"; repo = "nixpkgs"; ref = "nixos-20.03"; };
+
+  outputs = { self, nixpkgs }@inputs:
+    let
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+
+    in
+    {
+
+      # A Nixpkgs overlay.
+      overlay = final: prev:
+        with final;
+        {
+
+          lapack = prev.lapack.override { lapackProvider = final.mkl; };
+          blas = prev.blas.override { blasProvider = final.mkl; };
+
+          mangaki = callPackage ./nix/pkgs/mangaki { };
+
+        };
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system})
+            mangaki;
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.mangaki);
+
+      # A NixOS module, if applicable (e.g. if the package provides a system service).
+      nixosModules.mangaki = import ./nix/modules/mangaki.nix;
+
+      # Tests run by 'nix flake check' and by Hydra.
+      checks = forAllSystems (system: self.packages.${system});
+    };
+}
--- nix/nixpkgs-overlays.nix
+++
@@ -1,4 +0,0 @@
-self: super: {
-  lapack = super.lapack.override { lapackProvider = self.mkl; };
-  blas = super.blas.override { blasProvider = self.mkl; };
-}
