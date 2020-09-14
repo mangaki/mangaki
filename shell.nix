d8b4e35dd1083fda73c3cd1c806cab6a1ca5c968
Edmund Wu <fangkazuto@gmail.com>
2020-09-14 12:01:11 -0400

    nix: pin github:edolstra/flake-compat

--- shell.nix
+++ shell.nix
@@ -1,3 +1,9 @@
-(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
-  src = builtins.fetchGit ./.;
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash; }
+) {
+  src = ./.;
 }).shellNix
