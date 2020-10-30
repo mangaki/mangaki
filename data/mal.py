70359509dff96ba1b635969f5860ed52c6ea44c7
Xav <aurixav9@gmail.com>
2020-10-30 17:54:26 +0100

    mise en conformité

--- data/mal.py
+++ data/mal.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 # print(urlopen('http://myanimelist.net/topanime.php?type=bypopularity').read())
 
-b = BeautifulSoup(open('top.html').read()) # urllib2.urlopen('http://myanimelist.net/topanime.php?type=bypopularity')
+b = BeautifulSoup(open('top.html').read())  # urllib2.urlopen('http://myanimelist.net/topanime.php?type=bypopularity')
 for line in b.select('#content div table tr'):
     tds = line.findAll('td')
     print(tds[2].a.text)
--- data/search.py
+++ data/search.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 #print(urlopen('http://www.animeka.com/search/index.html?req=%s' % sys.argv[1]).read())
 
-b = BeautifulSoup(urlopen('http://www.animeka.com/search/index.html?req=%s' % sys.argv[1])) # &go_search=1&cat=search&zone_series=1&zone_episodes=1&zone_studios=1&zone_pers=1&zone_seriesf=1&zone_rlz=1&zone_team=1&type_search=all
+b = BeautifulSoup(urlopen('http://www.animeka.com/search/index.html?req=%s' % sys.argv[1]))  # &go_search=1&cat=search&zone_series=1&zone_episodes=1&zone_studios=1&zone_pers=1&zone_seriesf=1&zone_rlz=1&zone_team=1&type_search=all
 results = list(b.select('.animestxt a'))
 if len(results) >= 1:
     for line in results:
