78188239fd81b123ebb7dd6644183858407c81af
Raito Bezarius <masterancpp@gmail.com>
2021-10-17 18:24:41 +0200

    nix(overlays): just reuse the built-in composition

--- nix/pkgs/mangaki/default.nix
+++ nix/pkgs/mangaki/default.nix
@@ -1,7 +1,10 @@
-{ pkgs }:
+{ pkgs ? import <nixpkgs> {} }:
 pkgs.poetry2nix.mkPoetryApplication rec {
   src = ./../../..; # prevents unnecessary sanitizing which causes problems
   projectDir = src; # so it can find pyproject.toml and poetry.lock
-  overrides = pkgs.poetry2nix.overrides.withDefaults
-  (import ./overrides.nix { inherit pkgs; });
+  overrides = [
+    pkgs.poetry2nix.defaultPoetryOverrides
+    (import ./poetry-standard-overlay.nix)
+    (import ./poetry-git-overlay.nix { inherit pkgs; })
+  ];
 }
