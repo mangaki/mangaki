2fa2f40a9c148000d75b6c3e2ed822799bf3842f
Jill-Jênn Vie <vie@jill-jenn.net>
2022-08-01 16:32:56 +0200

    Remove comments

--- mangaki/mangaki/management/commands/index.py
+++ mangaki/mangaki/management/commands/index.py
@@ -21,16 +21,3 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         reindex()
-
-        '''work = Work.objects.get(id=26931)  # Death Note
-                                print('diff???', work.title_search, work.titles_search)
-                                # 'death':1 'note':2 vs. 'death':1A 'not':2A
-                                print('works', Work.objects.filter(title__mangaki_search='death note'))
-                        
-                                # Work.objects.filter(title_search='Death Note'))  # Is empty
-                                print('works', Work.objects.filter(titles_search='Death Note'))
-                                print('test', Work.objects.filter(title__search='Death Note'))
-                        
-                                # Work.objects.filter(title_search='tower of god'))  # Is empty
-                                print('works', Work.objects.filter(titles_search='tower of god').values('id'))
-                                print('test', Work.objects.filter(title__search='tower of god'))'''
